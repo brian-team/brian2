a826f8a7a373006b8b9d313e0e90ffc498d07905
Marcel Stimberg <marcel.stimberg@ens.fr>
2012-11-27 15:17:27 +0100

    add a simple setup script and a missing __init__.py file to the tests directory

---
+++ setup.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+'''
+Preliminary Brian2 setup script
+'''
+
+from distutils.core import setup
+
+setup(name='Brian2',
+      version='2.0dev',
+      packages=['brian2',
+                'brian2.codegen',
+                'brian2.codegen.languages',
+                'brian2.core',
+                'brian2.equations',
+                'brian2.groups',
+                'brian2.memory',
+                'brian2.monitors',
+                'brian2.sphinxext',
+                'brian2.stateupdaters',
+                'brian2.tests',
+                'brian2.units',
+                'brian2.utils'],
+     )
+
