c4736a3d3fbf35060256446a1b6d4fa2eb558c62
Marcel Stimberg <marcel.stimberg@ens.fr>
2013-01-16 19:33:58 +0100

    Delete output directory before creating the docs

--- dev/tools/docs/build_html_brian2.py
+++ dev/tools/docs/build_html_brian2.py
@@ -1,9 +1,10 @@
 import os
+import shutil
 import generate_references
 # first generate the reference documentation
 abs_root = os.path.abspath('../../../brian2')
 generate_references.main(abs_root, ['tests'], '../../../docs_sphinx/reference')
 
 os.chdir('../../../docs_sphinx')
-
+shutil.rmtree('../docs')
 os.system('sphinx-build -b html . ../docs')
