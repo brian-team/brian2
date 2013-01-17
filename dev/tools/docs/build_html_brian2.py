5fe81813ecb8a6a05d7e463f4673d4484c23697e
Marcel Stimberg <marcel.stimberg@ens.fr>
2013-01-17 17:10:58 +0100

    rename generate_references to generate_reference

--- dev/tools/docs/build_html_brian2.py
+++ dev/tools/docs/build_html_brian2.py
@@ -1,9 +1,9 @@
 import os
 import shutil
-import generate_references
+import generate_reference
 # first generate the reference documentation
 abs_root = os.path.abspath('../../../brian2')
-generate_references.main(abs_root, ['tests'], '../../../docs_sphinx/reference')
+generate_reference.main(abs_root, ['tests'], '../../../docs_sphinx/reference')
 
 os.chdir('../../../docs_sphinx')
 shutil.rmtree('../docs')
