d2bc412f4b494702690d3680587371c15c2b50d7
Marcel Stimberg <marcel.stimberg@ens.fr>
2013-02-06 18:50:06 +0100

    Add a test function to brian2, i.e. you can call brian2.test() to run the test suite

---
+++ brian2/tests/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+def run():
+    '''
+    Run brian's test suite. Needs an installation of the nose testing tool.
+    '''
+    import nose
+    from nose.plugins.doctests import Doctest
+    
+    dirname = os.path.join(os.path.dirname(__file__), '..')
+    return nose.run(argv=['', dirname, '--with-doctest'])
