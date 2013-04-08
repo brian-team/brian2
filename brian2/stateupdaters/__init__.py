30d0de571eb9082ff16a0340adb0263a76846674
Marcel Stimberg <marcel.stimberg@ens.fr>
2013-04-08 16:09:35 +0200

    Add a "constant" attribute to some specifiers to distinguish for example dt (which does not change during a run) from t (which changes every timestep).

--- brian2/stateupdaters/__init__.py
+++ brian2/stateupdaters/__init__.py
@@ -3,4 +3,5 @@ Module for transforming model equations into "abstract code" that can be then be
 further translated into executable code by the `codegen` module.
 '''  
 from .base import *
+from .exact import *
 from .integration import *
