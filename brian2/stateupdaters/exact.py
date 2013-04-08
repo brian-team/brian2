99047b73522f4ad008be73c3b86cc3fc10c11dff
Marcel Stimberg <marcel.stimberg@ens.fr>
2013-04-08 15:07:04 +0200

    Start implementing a linear state updater

--- brian2/stateupdaters/exact.py
+++ brian2/stateupdaters/exact.py
@@ -4,6 +4,9 @@ from sympy import Wild, Symbol, sympify
 import sympy as sp
 
 from brian2.equations import Equations
+from brian2.stateupdaters.base import StateUpdateMethod
+
+___all__ = ['linear']
 
 def get_linear_system(eqs):
     diff_eqs = eqs.substituted_expressions
@@ -39,34 +42,52 @@ def get_linear_system(eqs):
     return (diff_eq_names, coefficients, constants)
 
 
+class LinearStateUpdater(StateUpdateMethod):    
+    
+    def can_integrate(self, equations, namespace, specifiers):
+        return False
+    
+    def __call__(self, equations):
+        # Get a representation of the ODE system in the form of
+        # dX/dt = M*X + B
+        variables, matrix, constants = get_linear_system(eqs)
+        
+        
+        symbols = [Symbol(variable) for variable in variables]
+        solution = sp.solve_linear_system(matrix.row_join(constants), *symbols)
+        b = sp.Matrix([solution[symbol] for symbol in symbols]).transpose()
+        
+        # Solve the system
+        dt = Symbol('dt')    
+        A = (matrix * dt).exp()                
+        C = -sp.Matrix([A.dot(b)]) + b
+        S = sp.MatrixSymbol('_S', len(variables), 1)
+        updates = A * S + C.transpose()
+        
+        # The solution contains _S_00, _S_10 etc. for the state variables,
+        # replace them with the state variable names 
+        abstract_code = []
+        for idx, (variable, update) in enumerate(zip(variables, updates)):
+            abstract_code.append(variable + ' = ' +
+                                 str(update.subs('_S_%d0' % idx, variable)))
+        
+        return '\n'.join(abstract_code)
+
+linear = LinearStateUpdater()
+
+# The linear state updater has the highest priority
+StateUpdateMethod.register('linear', linear, 0)
 
 if __name__ == '__main__':
-    from brian2 import sin  
     eqs = Equations('''
-    dv/dt = sin(2*3.141*t*freq+phase) / tau : 1
+    dv/dt = -v / tau + const_v: 1
+    du/dt = -u / tau + const_u: 1
     tau : second
-    freq : Hz 
-    phase : 1
+    const_v : 1
+    const_u : 1
     ''' )
     
-    variables, matrix, b = get_linear_system(eqs)
-    print 'variables: ', variables
-    print '\nCoefficient matrix:'
-    print matrix
-    print '\nVector b:'
-    print b
-    print '\n diagonalized:'
-    t = Symbol('t')    
-    A = (matrix * t).exp()
-    print 'A = :'
-    print A
-    C = sp.Matrix([A.dot(b)]) + b
-    print 'C = :'
-    print C
-#    x(t) = dot(A(t), S(0)) + C(t)
-#    x(t + dt) = dot(A(t + dt), S(0)) + C(t + dt)
-#    = dot(A(t), S(0)) + dot(A(dt), S(0)) + C(t) + C(dt)
-#    = S(t) + dot(A(dt), S(0)) + C(dt)
-#    
-#    C(0) = 0
-        
+    print 'Equations:'
+    print eqs
+    print 'Abstract code'
+    print linear(eqs)
