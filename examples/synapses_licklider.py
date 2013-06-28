f546fdfb876b9456d45ba68a1731221095a23593
Marcel Stimberg <marcel.stimberg@ens.fr>
2013-06-28 18:14:11 +0200

    Add a new example: Licklider model for Pitch perception

---
+++ examples/synapses_licklider.py
@@ -0,0 +1,50 @@
+'''
+Spike-based adaptation of Licklider's model of pitch processing (autocorrelation with
+delay lines) with phase locking.
+
+Romain Brette
+'''
+from pylab import *
+from brian2 import *
+
+language = CPPLanguage()
+#language = PythonLanguage()
+
+defaultclock.dt = .02 * ms
+
+# Ear and sound
+max_delay = 20 * ms # 50 Hz
+tau_ear = 1 * ms
+sigma_ear = .1
+eqs_ear = '''
+dx/dt=(sound-x)/tau_ear+sigma_ear*(2./tau_ear)**.5*xi : 1 (active)
+sound=5*sin(2*pi*frequency*t)**3 : 1 # nonlinear distorsion
+#sound=5*(sin(4*pi*frequency*t)+.5*sin(6*pi*frequency*t)) : 1 # missing fundamental
+frequency=(200+200*t*Hz)*Hz : Hz # increasing pitch
+'''
+receptors = NeuronGroup(2, eqs_ear, threshold='x>1', reset='x=0', language=language)
+receptors.refractory = 2*ms
+# Coincidence detectors
+min_freq = 50 * Hz
+max_freq = 1000 * Hz
+N = 300
+tau = 1 * ms
+sigma = .1
+eqs_neurons = '''
+dv/dt=-v/tau+sigma*(2./tau)**.5*xi : 1
+'''
+
+neurons = NeuronGroup(N, eqs_neurons, threshold='v>1', reset='v=0', language=language)
+
+synapses = Synapses(receptors, neurons, 'w : 1', pre='v+=w', language=language)
+synapses.connect(True)
+synapses.w=0.5
+synapses.delay[1, :] = 1. / exp(linspace(log(min_freq / Hz), log(max_freq / Hz), N)) * second
+
+spikes = SpikeMonitor(neurons)
+
+run(500 * ms)
+plot(spikes.t, spikes.i, '.', mew=0)
+ylabel('Frequency')
+yticks([0, 99, 199, 299], array(1. / synapses.delay[1, [0, 99, 199, 299]], dtype=int))
+show()
