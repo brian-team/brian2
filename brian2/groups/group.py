dbeefa24ee8f24c88262de1d8c92fa4832cb744e
mstimberg <marcel.stimberg@ens.fr>
2013-08-25 21:38:02 +0200

    Remove a TODO comment that no longer applies

--- brian2/groups/group.py
+++ brian2/groups/group.py
@@ -200,8 +200,6 @@ class Group(object):
         abstract_code = variable.name + ' = ' + code
         namespace = get_local_namespace(level + 1)
         additional_namespace = ('implicit-namespace', namespace)
-        # TODO: Find a name that makes sense for reset and variable setting
-        # with code
         additional_variables = self.item_mapping.variables
         additional_variables['_group_idx'] = ArrayVariable('_group_idx',
                                                      Unit(1),
