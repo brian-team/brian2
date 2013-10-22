cae34df40a22979249b2ed14c41f8bf5659d4c5b
thesamovar <dan.goodman@ens.fr>
2013-10-22 18:12:09 -0400

    C++ standalone: can use saved arrays to initialise variables, but quite limited for the moment

--- brian2/devices/cpp_standalone/device.py
+++ brian2/devices/cpp_standalone/device.py
@@ -54,7 +54,12 @@ class StandaloneVariableView(VariableView):
 
     # Overwrite methods to signal that they are not available for standalone
     def set_array_with_array_index(self, item, value):
-        raise NotImplementedError(('Cannot set variables this way in '
+        if isinstance(item, slice) and item==slice(None) and isinstance(value, numpy.ndarray):
+            arrayname = self.group.variables[self.name].arrayname
+            staticarrayname = cpp_standalone_device.static_array(arrayname, value)
+            cpp_standalone_device.main_queue.append(('set_by_array', (arrayname, staticarrayname, item, value)))
+        else:
+            raise NotImplementedError(('Cannot set variables this way in '
                                    'standalone, try using string expressions.'))
 
     def __getitem__(self, item):
@@ -225,6 +230,15 @@ class CPPStandaloneDevice(Device):
             elif func=='run_network':
                 net, netcode = args
                 main_lines.extend(netcode.split('\n'))
+            elif func=='set_by_array':
+                arrayname, staticarrayname, item, value = args
+                code = '''
+                for(int i=0; i<_num__static_array_{staticarrayname}; i++)
+                {{
+                    {arrayname}[i] = _static_array_{staticarrayname}[i];
+                }}
+                '''.format(arrayname=arrayname, staticarrayname=staticarrayname)
+                main_lines.extend(code.split('\n'))
             else:
                 raise NotImplementedError("Unknown main queue function type "+func)
 
--- dev/ideas/devices/cpp_standalone.py
+++ dev/ideas/devices/cpp_standalone.py
@@ -34,7 +34,10 @@ G = NeuronGroup(N, eqs,
                 refractory=refractory,
                 name='gp')
 G.V['i>500'] = '-i*mV'
-cpp_standalone_device.static_array('test', array([1.,2.]))
+#cpp_standalone_device.static_array('test', array([1.,2.]))
+u = zeros(N)
+u[[1, 2, 3, 4]] = [3.14, 2.78, 1.41, 6.66]
+G.u = u
 #G.u[[1, 2]] = [3.14, 2.78]
 #G.u[array([3, 4])] = array([1.41, 6.66])
 M = SpikeMonitor(G)
