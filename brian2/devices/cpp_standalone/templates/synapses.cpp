6c822753be7e81a4f4f09aed85c7837f1f402c38
Marcel Stimberg <marcel.stimberg@ens.fr>
2013-11-18 15:30:03 +0100

    Use the int32_t type in standalone as well.

--- brian2/devices/cpp_standalone/templates/synapses.cpp
+++ brian2/devices/cpp_standalone/templates/synapses.cpp
@@ -2,15 +2,15 @@
 
 {% block maincode %}
 	{% if pathway is defined %}
-	vector<int> *_spiking_synapses = {{pathway.name}}.queue->peek();
-	const int _num_spiking_synapses = _spiking_synapses->size();
+	vector<int32_t> *_spiking_synapses = {{pathway.name}}.queue->peek();
+	const unsigned int _num_spiking_synapses = _spiking_synapses->size();
 	{% endif %}
-	for(int _spiking_synapse_idx=0;
+	for(unsigned int _spiking_synapse_idx=0;
 		_spiking_synapse_idx<_num_spiking_synapses;
 		_spiking_synapse_idx++)
 	{
-		const int _idx = (*_spiking_synapses)[_spiking_synapse_idx];
-		const int _vectorisation_idx = _idx;
+		const int32_t _idx = (*_spiking_synapses)[_spiking_synapse_idx];
+		const int32_t _vectorisation_idx = _idx;
 		{% for line in code_lines %}
 		{{line}}
 		{% endfor %}
