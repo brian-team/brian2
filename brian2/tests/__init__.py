b9eb2f338aa432f90539602e0b267844f3874edf
Marcel Stimberg <marcel.stimberg@ens.fr>
2013-12-18 16:44:33 +0100

    Run test suite for executable files (.py files get often installed as executable by distribute/setuptools)

--- brian2/tests/__init__.py
+++ brian2/tests/__init__.py
@@ -1,5 +1,6 @@
 import os
 
+
 def run():
     '''
     Run brian's test suite. Needs an installation of the nose testing tool.
@@ -9,9 +10,9 @@ def run():
     except ImportError:
         raise ImportError('Running the test suite requires the "nose" package.')
     
-    dirname = os.path.join(os.path.dirname(__file__), '..')
-    return nose.run(argv=['', dirname, '--with-doctest', '--nologcapture'])
+    dirname = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    print 'Running tests in "%s"' % dirname
+    return nose.run(argv=['', dirname, '--with-doctest', '--nologcapture', '--exe'])
 
 if __name__=='__main__':
     run()
-    
