95ea9da06b57c9234bde07678ad3d000b71d239e
thesamovar <dan.goodman@ens.fr>
2014-02-13 17:44:58 -0500

    synapses_create now sorts on variable attribute

--- brian2/devices/cpp_standalone/templates/synapses_create.cpp
+++ brian2/devices/cpp_standalone/templates/synapses_create.cpp
@@ -41,7 +41,7 @@
 
 	// now we need to resize all registered variables
 	const int newsize = {{_dynamic__synaptic_pre}}.size();
-	{% for variable in owner._registered_variables | sort %}
+	{% for variable in owner._registered_variables | sort(attribute='name') %}
 	{% set varname = get_array_name(variable, access_data=False) %}
 	{{varname}}.resize(newsize);
 	{% endfor %}
