b2f70945e5cd055d816db40a3e1622dc22e2c763
Marcel Stimberg <marcel.stimberg@ens.fr>
2014-02-20 22:44:09 +0100

    Fix mix of tabs and spaces in synapses_create.cpp template

--- brian2/codegen/runtime/weave_rt/templates/synapses_create.cpp
+++ brian2/codegen/runtime/weave_rt/templates/synapses_create.cpp
@@ -5,18 +5,18 @@
     USES_VARIABLES { _synaptic_pre, _synaptic_post, rand,
                      N_incoming, N_outgoing }
     #}
-	srand((unsigned int)time(NULL));
-	const int _buffer_size = 1024;
-	int *const _prebuf = new int[_buffer_size];
-	int *const _postbuf = new int[_buffer_size];
-	int *const _synprebuf = new int[1];
-	int *const _synpostbuf = new int[1];
-	int _curbuf = 0;
-	for(int i=0; i<_num_all_pre; i++)
-	{
-		for(int j=0; j<_num_all_post; j++)
-		{
-		    const int _vectorisation_idx = j;
+    srand((unsigned int)time(NULL));
+    const int _buffer_size = 1024;
+    int *const _prebuf = new int[_buffer_size];
+    int *const _postbuf = new int[_buffer_size];
+    int *const _synprebuf = new int[1];
+    int *const _synpostbuf = new int[1];
+    int _curbuf = 0;
+    for(int i=0; i<_num_all_pre; i++)
+    {
+        for(int j=0; j<_num_all_post; j++)
+        {
+            const int _vectorisation_idx = j;
             {# The abstract code consists of the following lines (the first two lines
             are there to properly support subgroups as sources/targets):
             _pre_idx = _all_pre
@@ -25,20 +25,20 @@
             _n = {user-specified number of synapses}
             _p = {user-specified probability}
             #}
-		    {{ super() }}
-			// Add to buffer
-			if(_cond)
-			{
-			    if (_p != 1.0) {
-			        // We have to use _rand instead of rand to use our rand
-			        // function, not the one from the C standard library
-			        if (_rand(_vectorisation_idx) >= _p)
-			            continue;
-			    }
+            {{ super() }}
+            // Add to buffer
+            if(_cond)
+            {
+                if (_p != 1.0) {
+                    // We have to use _rand instead of rand to use our rand
+                    // function, not the one from the C standard library
+                    if (_rand(_vectorisation_idx) >= _p)
+                        continue;
+                }
 
-			    for (int _repetition=0; _repetition<_n; _repetition++) {
-			        {{N_outgoing}}[i] += 1;
-			        {{N_incoming}}[j] += 1;
+                for (int _repetition=0; _repetition<_n; _repetition++) {
+                    {{N_outgoing}}[i] += 1;
+                    {{N_incoming}}[j] += 1;
                     _prebuf[_curbuf] = _pre_idx;
                     _postbuf[_curbuf] = _post_idx;
                     _curbuf++;
@@ -50,42 +50,42 @@
                         _curbuf = 0;
                     }
                 }
-			}
-		}
-	}
-	// Final buffer flush
-	_flush_buffer(_prebuf, {{_dynamic__synaptic_pre}}, _curbuf);
-	_flush_buffer(_postbuf, {{_dynamic__synaptic_post}}, _curbuf);
+            }
+        }
+    }
+    // Final buffer flush
+    _flush_buffer(_prebuf, {{_dynamic__synaptic_pre}}, _curbuf);
+    _flush_buffer(_postbuf, {{_dynamic__synaptic_post}}, _curbuf);
 
-	const int newsize = {{_dynamic__synaptic_pre}}.size();
-	// now we need to resize all registered variables (via Python)
-	py::tuple _newlen_tuple(1);
-	_newlen_tuple[0] = newsize;
-	_owner.mcall("_resize", _newlen_tuple);
+    const int newsize = {{_dynamic__synaptic_pre}}.size();
+    // now we need to resize all registered variables (via Python)
+    py::tuple _newlen_tuple(1);
+    _newlen_tuple[0] = newsize;
+    _owner.mcall("_resize", _newlen_tuple);
 
-	delete [] _prebuf;
-	delete [] _postbuf;
-	delete [] _synprebuf;
-	delete [] _synpostbuf;
+    delete [] _prebuf;
+    delete [] _postbuf;
+    delete [] _synprebuf;
+    delete [] _synpostbuf;
 {% endblock %}
 
 {% block support_code_block %}
 // Flush a buffered segment into a dynamic array
 void _flush_buffer(int *buf, py::object &dynarr, int N)
 {
-	int _curlen = dynarr.attr("shape")[0];
-	int _newlen = _curlen+N;
-	// Resize the array
-	py::tuple _newlen_tuple(1);
-	_newlen_tuple[0] = _newlen;
-	dynarr.mcall("resize", _newlen_tuple);
-	// Get the potentially newly created underlying data arrays
-	int *data = (int*)(((PyArrayObject*)(PyObject*)dynarr.attr("data"))->data);
-	// Copy the values across
-	for(int i=0; i<N; i++)
-	{
-		data[_curlen+i] = buf[i];
-	}
+    int _curlen = dynarr.attr("shape")[0];
+    int _newlen = _curlen+N;
+    // Resize the array
+    py::tuple _newlen_tuple(1);
+    _newlen_tuple[0] = _newlen;
+    dynarr.mcall("resize", _newlen_tuple);
+    // Get the potentially newly created underlying data arrays
+    int *data = (int*)(((PyArrayObject*)(PyObject*)dynarr.attr("data"))->data);
+    // Copy the values across
+    for(int i=0; i<N; i++)
+    {
+        data[_curlen+i] = buf[i];
+    }
 }
 
 {{ super() }}
