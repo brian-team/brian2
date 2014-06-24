brian-team/brian2

commit c6851d752d7b7d3a5331bc4f4ac729543ea8969f

Store dependencies for objects that depend on other objects (monitors, synapses). Not including them in an explicitly constructed `Network` raises an error, not including them in a `MagicNetwork` (e.g. because a reference to a monitor is keeping that object alive) will raise a warning and set the object to inactive. Closes #92

brian2/tests/test_network.py
@@ -7,7 +7,9 @@ from nose import with_setup
 from brian2 import (Clock, Network, ms, second, BrianObject, defaultclock,
                     run, stop, NetworkOperation, network_operation,
                     restore_initial_state, MagicError, magic_network, clear,
-                    NeuronGroup, StateMonitor)
+                    NeuronGroup, StateMonitor, SpikeMonitor,
+                    PopulationRateMonitor)
+from brian2.utils.logger import catch_logs
 
 @with_setup(teardown=restore_initial_state)
 def test_empty_network():
@@ -351,6 +353,33 @@ def test_loop():
     assert v[0] == 0 and 0 < v[-1] < 1
 
 
+@with_setup(teardown=restore_initial_state)
+def test_loop2():
+    '''
+    Somewhat realistic test with a loop of magic networks and proxy objects
+    '''
+    updates[:] = []
+    import objgraph
+    def run_simulation():
+        G = NeuronGroup(10, 'dv/dt = -v / (10*ms) : 1',
+                        reset='v=0', threshold='v>1')
+        G.v = np.linspace(0, 1, 10)
+        v_mon = StateMonitor(G, 'v', record=True)
+        spike_mon = SpikeMonitor(G)
+        r_mon = PopulationRateMonitor(G)
+        run(1*ms)
+        # We return potentially problematic references to monitors
+        return v_mon, spike_mon, r_mon
+
+    # First run
+    v_mon, spike_mon, r_mon = run_simulation()
+
+    # Second run (we should get a warning for each monitor
+    with catch_logs() as l:
+        v_mon, spike_mon, r_mon = run_simulation()
+        assert (len(l) == 3 and
+                all([w[1] == 'brian2.core.magic.dependency_warning' for w in l]))
+
 if __name__=='__main__':
     for t in [test_empty_network,
               test_network_single_object,
@@ -367,7 +396,8 @@ if __name__=='__main__':
               test_network_copy,
               test_invalid_magic_network,
               test_network_access,
-              test_loop
+              test_loop,
+              test_loop2
               ]:
         t()
         restore_initial_state()

brian2/core/base.py
@@ -55,7 +55,8 @@ class BrianObject(Nameable):
 #        #: The `Clock` determining when the object should be updated.
 #        self.clock = clock
         self._clock = clock
-        
+
+        self._dependencies = set()
         self._contained_objects = []
         self._code_objects = []
         
@@ -74,6 +75,22 @@ class BrianObject(Nameable):
     #: `add_to_magic_network` to ``True``, but it should not be set for all the
     #: dependent objects such as `StateUpdater`
     add_to_magic_network = False
+
+    def add_dependency(self, obj):
+        '''
+        Add an object to the list of dependencies. Takes care of handling
+        subgroups correctly (i.e., adds its parent object).
+
+        Parameters
+        ----------
+        obj : `BrianObject`
+            The object that this object depends on.
+        '''
+        from brian2.groups.subgroup import Subgroup
+        if isinstance(obj, Subgroup):
+            self._dependencies.add(obj.source.id)
+        else:
+            self._dependencies.add(obj.id)
 
     def before_run(self, run_namespace=None, level=0):
         '''

brian2/core/magic.py
@@ -143,17 +143,19 @@ class MagicNetwork(Network):
         raise MagicError("Cannot directly modify MagicNetwork")
     
     def _update_magic_objects(self):
-        # Go through all the objects and ignore those that are only referred to
-        # by Proxy objects (e.g. because a Monitor holds a reference to them)
         valid_refs = set()
         all_objects = set()
         for obj in BrianObject.__instances__():
             obj = obj()
             if obj is None:
                 continue
-            all_objects.add(obj)
+            if obj.add_to_magic_network:
+                all_objects.add(obj)
+                for contained in _get_contained_objects(obj):
+                    all_objects.add(contained)
             if obj.invalidates_magic_network:
                 valid_refs.add(weakref.ref(obj))
+            del obj
 
         # check whether we should restart time, continue time, or raise an
         # error
@@ -173,6 +175,21 @@ class MagicNetwork(Network):
                      "with names {names}".format(
                         numobjs=len(self.objects),
                         names=', '.join(obj.name for obj in self.objects)))
+
+    def check_dependencies(self):
+        all_ids = set([obj.id for obj in self.objects])
+        for obj in self.objects:
+            if not obj.active:
+                continue  # object is already inactive, no need to check it
+            for dependency in obj._dependencies:
+                if not dependency in all_ids:
+                    logger.warn(('"%s" has been included in the network but '
+                                 'not the object on which it depends.'
+                                 'Setting "%s" to inactive.') % (obj.name,
+                                                                 obj.name),
+                                name_suffix='dependency_warning')
+                    obj.active = False
+                    break
 
     def before_run(self, run_namespace=None, level=0):
         self._update_magic_objects()

brian2/core/names.py
@@ -1,3 +1,5 @@
+import uuid
+
 from brian2.utils.logger import get_logger
 from brian2.core.tracking import Trackable
 import re
@@ -54,6 +56,7 @@ class Nameable(Trackable):
         If the name is already taken.
     '''    
     def __init__(self, name):
+        self._id = uuid.uuid4()
         if not isinstance(name, basestring):
             raise TypeError(('"name" argument has to be a string, is type '
                              '{type} instead').format(type=repr(type(name))))
@@ -71,6 +74,16 @@ class Nameable(Trackable):
                         variable name, i.e. starting with a letter
                         character and followed by alphanumeric characters and
                         ``_``.
+                        ''')
+
+    id = property(fget=lambda self:self._id,
+                    doc='''
+                        A unique id for this object.
+
+                        In contrast to names, which may be reused, the id stays
+                        unique. This is used in the dependency checking to not
+                        have to deal with the chore of comparing weak
+                        references, weak proxies and strong references.
                         ''')
 
     

brian2/core/network.py
@@ -137,6 +137,12 @@ class Network(Nameable):
 
         raise KeyError('No object with name "%s" found' % key)
 
+    def __contains__(self, item):
+        for obj in self.objects:
+            if obj.name == item:
+                return True
+        return False
+
     def __len__(self):
         return len(self.objects)
 
@@ -251,7 +257,15 @@ class Network(Nameable):
         '''
         when_to_int = dict((when, i) for i, when in enumerate(self.schedule))
         self.objects.sort(key=lambda obj: (when_to_int[obj.when], obj.order))
-    
+
+    def check_dependencies(self):
+        for obj in self.objects:
+            for dependency in obj._dependencies:
+                if not dependency in self:
+                    raise ValueError(('"%s" has been included in the network '
+                                      'but not "%s" on which it '
+                                      'depends.') % (obj.name, dependency))
+
     @device_override('network_before_run')
     def before_run(self, run_namespace=None, level=0):
         '''
@@ -282,9 +296,12 @@ class Network(Nameable):
                         numobj=len(self.objects),
                         objnames=', '.join(obj.name for obj in self.objects)),
                      "before_run")
-        
+
+        self.check_dependencies()
+
         for obj in self.objects:
-            obj.before_run(run_namespace, level=level+2)
+            if obj.active:
+                obj.before_run(run_namespace, level=level+2)
 
         logger.debug("Network {self.name} has {num} "
                      "clocks: {clocknames}".format(self=self,
@@ -298,7 +315,8 @@ class Network(Nameable):
         after_run()
         '''
         for obj in self.objects:
-            obj.after_run()
+            if obj.active:
+                obj.after_run()
         
     def _nextclocks(self):
         minclock = min(self._clocks, key=lambda c: c.t_)

brian2/monitors/ratemonitor.py
@@ -1,3 +1,5 @@
+import weakref
+
 import numpy as np
 
 from brian2.core.scheduler import Scheduler
@@ -24,19 +26,21 @@ class PopulationRateMonitor(Group, CodeRunner):
     codeobj_class : class, optional
         The `CodeObject` class to run code with.
     '''
+    invalidates_magic_network = False
     add_to_magic_network = True
-
     def __init__(self, source, name='ratemonitor*',
                  codeobj_class=None):
 
         #: The group we are recording from
-        self.source = source
+        self.source = weakref.proxy(source)
 
         scheduler = Scheduler(clock=source.clock, when='end')
 
         self.codeobj_class = codeobj_class
         CodeRunner.__init__(self, group=self, template='ratemonitor',
                             when=scheduler, name=name)
+
+        self.add_dependency(source)
 
         self.variables = Variables(self)
         self.variables.add_reference('_spikespace',

brian2/monitors/spikemonitor.py
@@ -1,3 +1,5 @@
+import weakref
+
 import numpy as np
 
 from brian2.core.scheduler import Scheduler
@@ -29,12 +31,13 @@ class SpikeMonitor(Group, CodeRunner):
     codeobj_class : class, optional
         The `CodeObject` class to run code with.
     '''
+    invalidates_magic_network = False
     add_to_magic_network = True
     def __init__(self, source, record=True, when=None, name='spikemonitor*',
                  codeobj_class=None):
         self.record = bool(record)
         #: The source we are recording from
-        self.source = source
+        self.source = weakref.proxy(source)
 
         # run by default on source clock at the end
         scheduler = Scheduler(when)
@@ -46,6 +49,8 @@ class SpikeMonitor(Group, CodeRunner):
         self.codeobj_class = codeobj_class
         CodeRunner.__init__(self, group=self, template='spikemonitor',
                             name=name, when=scheduler)
+
+        self.add_dependency(source)
 
         # Handle subgroups correctly
         start = getattr(source, 'start', 0)

brian2/monitors/statemonitor.py
@@ -132,6 +132,7 @@ class StateMonitor(Group, CodeRunner):
         show()
 
     '''
+    invalidates_magic_network = False
     add_to_magic_network = True
     def __init__(self, source, variables, record=None, when=None,
                  name='statemonitor*', codeobj_class=None):
@@ -179,6 +180,8 @@ class StateMonitor(Group, CodeRunner):
                             code=code, name=name,
                             when=scheduler,
                             check_units=False)
+
+        self.add_dependency(source)
 
         # Setup variables
         self.variables = Variables(self)

commit feb1c9a73ee0eb990d0cf7a98fef43a90d48a623

Fix a bug that had been silently introduced in commit c6851d75: `CodeRunner` objects were not automatically added to magic networks.

brian2/groups/group.py
@@ -686,6 +686,8 @@ class CodeRunner(BrianObject):
         A list of variable names which are used as conditions (e.g. for
         refractoriness) which should be ignored.
     '''
+    add_to_magic_network = True
+    invalidates_magic_network = True
     def __init__(self, group, template, code='', when=None,
                  name='coderunner*', check_units=True, template_kwds=None,
                  needed_variables=None, override_conditional_write=None,

brian2/tests/test_network.py
@@ -10,7 +10,8 @@ from brian2 import (Clock, Network, ms, second, BrianObject, defaultclock,
                     run, stop, NetworkOperation, network_operation,
                     restore_initial_state, MagicError, clear, Synapses,
                     NeuronGroup, StateMonitor, SpikeMonitor,
-                    PopulationRateMonitor, MagicNetwork, magic_network)
+                    PopulationRateMonitor, MagicNetwork, magic_network,
+                    PoissonGroup, Hz, collect)
 from brian2.utils.logger import catch_logs
 
 def test_incorrect_network_use():
@@ -466,6 +467,25 @@ def test_loop():
         assert '4 objects' in magic_objects
 
 
+def test_magic_collect():
+    '''
+    Make sure all expected objects are collected in a magic network
+    '''
+    P = PoissonGroup(10, rates=100*Hz)
+    G = NeuronGroup(10, 'v:1')
+    S = Synapses(G, G, '')
+    G_runner = G.runner('')
+    S_runner = S.runner('')
+
+    state_mon = StateMonitor(G, 'v', record=True)
+    spike_mon = SpikeMonitor(G)
+    rate_mon = PopulationRateMonitor(G)
+
+    objects = collect()
+
+    assert len(objects) == 8, ('expected %d objects, got %d' % (8, len(objects)))
+
+
 if __name__=='__main__':
     for t in [test_incorrect_network_use,
               test_network_contains,
@@ -487,6 +507,7 @@ if __name__=='__main__':
               test_invalid_magic_network,
               test_network_access,
               test_loop,
+              test_magic_collect
               ]:
         t()
         restore_initial_state()