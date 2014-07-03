5d1337648856e9d0c6d6d8f4bb390490221a98dd
Marcel Stimberg <marcel.stimberg@ens.fr>
2014-07-03 22:46:22 +0200

    Fix synapses creation from arrays for weave: the code did not use the underlying data array

--- brian2/codegen/runtime/weave_rt/templates/synapses_create_array.cpp
+++ brian2/codegen/runtime/weave_rt/templates/synapses_create_array.cpp
@@ -12,14 +12,17 @@ const int _new_num_synapses = _old_num_synapses + _numsources;
 _newlen_tuple[0] = _new_num_synapses;
 {{_dynamic__synaptic_pre}}.mcall("resize", _newlen_tuple);
 {{_dynamic__synaptic_post}}.mcall("resize", _newlen_tuple);
+// Get the potentially newly created underlying data arrays
+int *_synaptic_pre_data = (int*)(((PyArrayObject*)(PyObject*){{_dynamic__synaptic_pre}}.attr("data"))->data);
+int *_synaptic_post_data = (int*)(((PyArrayObject*)(PyObject*){{_dynamic__synaptic_post}}.attr("data"))->data);
 
 for (int _idx=0; _idx<_numsources; _idx++) {
     {# After this code has been executed, the arrays _real_sources and
        _real_variables contain the final indices. Having any code here it all is
        only necessary for supporting subgroups #}
     {{vector_code|autoindent}}
-    {{_dynamic__synaptic_pre}}[_idx + _old_num_synapses] = _real_sources;
-    {{_dynamic__synaptic_post}}[_idx + _old_num_synapses] = _real_targets;
+    _synaptic_pre_data[_idx + _old_num_synapses] = _real_sources;
+    _synaptic_post_data[_idx + _old_num_synapses] = _real_targets;
     // Update the number of total outgoing/incoming synapses per source/target neuron
     {{N_outgoing}}[_real_sources]++;
     {{N_incoming}}[_real_targets]++;
