842272ec831dfc3708a39896e34f59317cba1e40
Romain Brette <romain.brette@ens.fr>
2014-08-09 14:30:30 +0200

    New SpatialNeuron example: short cylinder

---
+++ examples/compartmental/cylinder.py
@@ -0,0 +1,40 @@
+'''
+A short cylinder with constant injection at one end.
+'''
+from brian2 import *
+
+defaultclock.dt = 0.01*ms
+
+brian_prefs.codegen.target = 'weave'
+
+# Morphology
+diameter = 1*um
+length = 300*um
+Cm = 1 * uF / cm ** 2
+Ri = 150 * ohm * cm
+N = 200
+morpho=Cylinder(diameter=diameter,length=length,n=N)
+
+# Passive channels
+gL=1e-4*siemens/cm**2
+EL=-70*mV
+eqs='''
+Im=gL*(EL-v) : amp/meter**2
+I : amp (point current)
+'''
+
+neuron = SpatialNeuron(morphology=morpho, model=eqs, Cm=Cm, Ri=Ri)
+neuron.v = EL
+
+taum = Cm/gL # membrane time constant
+print "Time constant",taum
+rm = 1/(gL * pi * diameter) # membrane resistance per unit length
+ra = (4 * Ri)/(pi * diameter**2) # axial resistance per unit length
+la = sqrt(rm/ra) # space length
+print "Characteristic length",la
+
+neuron.I[0]=0.02*nA # injecting at the left end
+run(50*ms,report='text')
+
+plot(neuron.distance/um,neuron.v/mV,'k')
+show()
