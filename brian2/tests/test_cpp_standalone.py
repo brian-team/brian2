3de5d89b541b9a45b9d5aedfacbb7b12e1d57304
Marcel Stimberg <marcel.stimberg@ens.fr>
2014-09-10 15:48:32 +0200

    Use `char` to store boolean values in C++ standalone. Fixes #317

--- brian2/tests/test_cpp_standalone.py
+++ brian2/tests/test_cpp_standalone.py
@@ -3,6 +3,7 @@ import os
 
 from nose import with_setup
 import numpy
+from numpy.testing.utils import assert_allclose
 
 from brian2 import *
 from brian2.devices.cpp_standalone import cpp_standalone_device
@@ -69,12 +70,52 @@ def test_multiple_connects(with_output=False):
     assert len(S) == 2 and len(S.w[:]) == 2
 
 
+@with_setup(teardown=restore_device)
+def test_storing_loading(with_output=False):
+    set_device('cpp_standalone')
+    G = NeuronGroup(10, '''v : volt
+                           x : 1
+                           n : integer
+                           b : boolean''')
+    v = np.arange(10)*volt
+    x = np.arange(10, 20)
+    n = np.arange(20, 30)
+    b = np.array([True, False]).repeat(5)
+    G.v = v
+    G.x = x
+    G.n = n
+    G.b = b
+    S = Synapses(G, G, '''v : volt
+                          x : 1
+                          n : integer
+                          b : boolean''', connect='i==j')
+    S.v = v
+    S.x = x
+    S.n = n
+    S.b = b
+    run(0*ms)
+    tempdir = tempfile.mkdtemp()
+    if with_output:
+        print tempdir
+    device.build(project_dir=tempdir, compile_project=True, run_project=True,
+                 with_output=True)
+    assert_allclose(G.v[:], v)
+    assert_allclose(S.v[:], v)
+    assert_allclose(G.x[:], x)
+    assert_allclose(S.x[:], x)
+    assert_allclose(G.n[:], n)
+    assert_allclose(S.n[:], n)
+    assert_allclose(G.b[:], b)
+    assert_allclose(S.b[:], b)
+
+
 if __name__=='__main__':
     # Print the debug output when testing this file only but not when running
     # via nose test
     for t in [
              test_cpp_standalone,
-             test_multiple_connects
+             test_multiple_connects,
+             test_storing_loading
              ]:
         t(with_output=True)
         restore_device()
