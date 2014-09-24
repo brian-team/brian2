dfcff300b1ae57ca551077db0b3dd610d9fd5147
thesamovar <dan.goodman@ens.fr>
2014-09-24 20:54:07 +0100

    Make Cython optional

--- brian2/codegen/generators/__init__.py
+++ brian2/codegen/generators/__init__.py
@@ -1,4 +1,8 @@
 from .base import *
 from .cpp_generator import *
 from .numpy_generator import *
-from .cython_generator import *
+try:
+    from .cython_generator import *
+except ImportError:
+    pass # todo: raise a warning?
+
--- brian2/codegen/runtime/__init__.py
+++ brian2/codegen/runtime/__init__.py
@@ -4,4 +4,7 @@ Runtime targets for code generation.
 
 from .numpy_rt import *
 from .weave_rt import *
-from .cython_rt import *
+try:
+    from .cython_rt import *
+except ImportError:
+    pass # todo: raise a warning?
