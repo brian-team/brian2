feca9a0329db7c95a303f7282dcabfbb3a950dcf
Pierre Yger <pierre@yger.net>
2014-09-25 15:27:42 +0200

    Addition of a test checking the consistency of the results with multi-threading. A quite decently complex network is covering almost all the templates, and simulations results are carefully compared without OpenMP, with OpenMP and 1 or 2 threads.

--- brian2/tests/test_cpp_standalone.py
+++ brian2/tests/test_cpp_standalone.py
@@ -69,12 +69,98 @@ def test_multiple_connects(with_output=False):
     assert len(S) == 2 and len(S.w[:]) == 2
 
 
+@with_setup(teardown=restore_device)
+def test_openmp_consistency(with_output=False):
+
+    set_device('cpp_standalone')
+
+    n_cells    = 100
+    n_recorded = 10
+    numpy.random.seed(42)
+    taum       = 20 * ms
+    taus       = 5 * ms
+    Vt         = -50 * mV
+    Vr         = -60 * mV
+    El         = -49 * mV
+    fac        = (60 * 0.27 / 10)
+    gmax       = 20*fac
+    dApre      = .01
+    taupre     = 20 * ms
+    taupost    = taupre
+    dApost     = -dApre * taupre / taupost * 1.05
+    dApost    *=  0.1*gmax
+    dApre     *=  0.1*gmax
+
+    connectivity = numpy.random.randn(n_cells, n_cells)
+    sources      = numpy.random.random_integers(0, n_cells-1, 10*n_cells)
+    times        = 1*second*numpy.sort(numpy.random.rand(10*n_cells))
+    v_init       = Vr + numpy.random.rand(n_cells) * (Vt - Vr)
+
+    eqs  = Equations('''
+    dv/dt = (g-(v-El))/taum : volt
+    dg/dt = -g/taus         : volt
+    ''')
+
+    results = {}
+
+    for n_threads in [0, 1, 2]:
+        Synapses.__instances__().clear()
+        device.reinit()
+        brian_prefs.codegen.cpp_standalone.openmp_threads = n_threads                
+        P    = NeuronGroup(n_cells, model=eqs, threshold='v>Vt', reset='v=Vr', refractory=5 * ms)
+        Q    = SpikeGeneratorGroup(n_cells, sources, times)
+        P.v  = v_init
+        P.g  = 0 * mV
+        S    = Synapses(P, P, 
+                            model = '''dApre/dt=-Apre/taupre    : 1 (event-driven)    
+                                       dApost/dt=-Apost/taupost : 1 (event-driven)
+                                       w                        : 1''', 
+                            pre = '''g     += w*mV
+                                     Apre  += dApre
+                                     w      = w + Apost''',
+                            post = '''Apost += dApost
+                                      w      = w + Apre''',
+                            connect=True)
+        
+        S.w       = fac*connectivity.flatten()
+
+        T         = Synapses(Q, P, model = "w : 1", pre="g += w*mV", connect='i==j')
+        T.w       = 10*fac
+
+        spike_mon = SpikeMonitor(P)
+        rate_mon  = PopulationRateMonitor(P)
+        state_mon = StateMonitor(S, 'w', record=range(n_recorded), when=Clock(dt=0.1*second))
+        v_mon     = StateMonitor(P, 'v', record=range(n_recorded))
+
+        run(0.2 * second, report='text')
+
+        tempdir = tempfile.mkdtemp()
+        if with_output:
+            print tempdir
+        device.build(project_dir=tempdir, compile_project=True, run_project=True,
+                     with_output=with_output)
+
+        results[n_threads]      = {}
+        results[n_threads]['w'] = state_mon.w
+        results[n_threads]['v'] = v_mon.v
+        results[n_threads]['s'] = spike_mon.num_spikes
+        results[n_threads]['r'] = rate_mon.rate[:]
+        print results[n_threads]
+
+    for n_threads in [1, 2]:
+        assert (results[n_threads]['w'] == results[0]['w']).all()
+        assert (results[n_threads]['v'] == results[0]['v']).all()
+        assert (results[n_threads]['r'] == results[0]['r']).all()
+        assert (results[n_threads]['s'] == results[0]['s'])
+
+
 if __name__=='__main__':
     # Print the debug output when testing this file only but not when running
     # via nose test
     for t in [
              test_cpp_standalone,
-             test_multiple_connects
+             test_multiple_connects,
+             test_openmp_consistency
              ]:
         t(with_output=True)
         restore_device()
