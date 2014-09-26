ba746a34beac791f645db9643efdf2ce4807e7cf
thesamovar <dan.goodman@ens.fr>
2014-09-26 21:36:34 +0100

    Isolated problem with state monitor (and maybe refractoriness): booleans in Cython

--- dev/ideas/cython/testing_templates.py
+++ dev/ideas/cython/testing_templates.py
@@ -3,7 +3,6 @@ from brian2 import *
 #brian_prefs.codegen.target = 'weave'
 brian_prefs.codegen.target = 'cython'
 
-G = SpikeGeneratorGroup(2, [0, 1, 0, 1], [0*ms, 1*ms, 2*ms, 3*ms])
-M = SpikeMonitor(G)
-run(4*ms)
-print M.i, M.t
+G = NeuronGroup(2, 'v:boolean')
+M = StateMonitor(G, 'v', record=True)
+run(10*ms)
