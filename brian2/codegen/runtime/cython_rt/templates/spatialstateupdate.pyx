d9ff1308dec98dccc3ad2f649d23d1f36321e880
Marcel Stimberg <marcel.stimberg@ens.fr>
2014-09-29 16:46:42 +0200

    Fix spatialstateupdate template in Cython (range(N-2, 0, -1) --> range(N-2, -1, 1) to include the 0).

--- brian2/codegen/runtime/cython_rt/templates/spatialstateupdate.pyx
+++ brian2/codegen/runtime/cython_rt/templates/spatialstateupdate.pyx
@@ -12,65 +12,65 @@
     cdef double bi
     cdef double _m
 
-    cdef int i
+    cdef int _i
 
     ## MAIN CODE ######
     # Tridiagonal solving
     # Pass 1
-    for i in range(N):
-        _idx = i
+    for _i in range(N):
+        _idx = _i
         _vectorisation_idx = _idx
 
         {{vector_code|autoindent}}
         _gtot_all[_idx] = _gtot
 
-        {{v_star}}[i] = -({{Cm}}[i]/dt*{{v}}[i])-_I0 # RHS -> v_star (solution)
-        bi={{ab_star}}[N + i] - _gtot_all[i] # main diagonal
-        if (i<N-1):
-            c[i]= {{ab_star}}[0 + i+1] # superdiagonal
-        if (i>0):
-            ai = {{ab_star}}[2*N + i-1] # subdiagonal
-            _m = 1.0/(bi-ai*c[i-1])
-            c[i] = c[i]*_m
-            {{v_star}}[i] = ({{v_star}}[i] - ai*{{v_star}}[i-1])*_m
+        {{v_star}}[_i] = -({{Cm}}[_i]/dt*{{v}}[_i])-_I0 # RHS -> v_star (solution)
+        bi={{ab_star}}[N + _i] - _gtot_all[_i] # main diagonal
+        if (_i<N-1):
+            c[_i]= {{ab_star}}[0 + _i+1] # superdiagonal
+        if (_i>0):
+            ai = {{ab_star}}[2*N + _i-1] # subdiagonal
+            _m = 1.0/(bi-ai*c[_i-1])
+            c[_i] = c[_i]*_m
+            {{v_star}}[_i] = ({{v_star}}[_i] - ai*{{v_star}}[_i-1])*_m
         else:
             c[0]=c[0]/bi
             {{v_star}}[0]={{v_star}}[0]/bi
-    for i in range(N-2, 0, -1):
-        {{v_star}}[i]={{v_star}}[i] - c[i]*{{v_star}}[i+1]
+    for _i in range(N-2, -1, -1):
+        {{v_star}}[_i]={{v_star}}[_i] - c[_i]*{{v_star}}[_i+1]
     
     # Pass 2
-    for i in range(N):
-        {{u_plus}}[i] = {{b_plus}}[i] # RHS -> v_star (solution)
-        bi = {{ab_plus}}[N + i]-_gtot_all[i] # main diagonal
-        if (i<N-1):
-            c[i] = {{ab_plus}}[0 + i+1] # superdiagonal
-        if (i>0):
-            ai = {{ab_plus}}[2*N + i-1] # subdiagonal
-            _m = 1.0/(bi-ai*c[i-1])
-            c[i] = c[i]*_m
-            {{u_plus}}[i] = ({{u_plus}}[i] - ai*{{u_plus}}[i-1])*_m
+    for _i in range(N):
+        {{u_plus}}[_i] = {{b_plus}}[_i] # RHS -> v_star (solution)
+        bi = {{ab_plus}}[N + _i]-_gtot_all[_i] # main diagonal
+        if (_i<N-1):
+            c[_i] = {{ab_plus}}[0 + _i+1] # superdiagonal
+        if (_i>0):
+            ai = {{ab_plus}}[2*N + _i-1] # subdiagonal
+            _m = 1.0/(bi-ai*c[_i-1])
+            c[_i] = c[_i]*_m
+            {{u_plus}}[_i] = ({{u_plus}}[_i] - ai*{{u_plus}}[_i-1])*_m
         else:
             c[0]=c[0]/bi
             {{u_plus}}[0] = {{u_plus}}[0]/bi
-    for i in range(N-2, 0, -1):
-        {{u_plus}}[i] = {{u_plus}}[i] - c[i]*{{u_plus}}[i+1]
+    for _i in range(N-2, -1, -1):
+        {{u_plus}}[_i] = {{u_plus}}[_i] - c[_i]*{{u_plus}}[_i+1]
     
     # Pass 3
-    for i in range(N):
-        {{u_minus}}[i] = {{b_minus}}[i] # RHS -> v_star (solution)
-        bi = {{ab_minus}}[N + i] - _gtot_all[i] # main diagonal
-        if (i<N-1):
-            c[i] = {{ab_minus}}[0 + i+1] # superdiagonal
-        if (i>0):
-            ai = {{ab_minus}}[2*N + i-1] # subdiagonal
-            _m = 1.0/(bi-ai*c[i-1])
-            c[i] = c[i]*_m
-            {{u_minus}}[i] = ({{u_minus}}[i] - ai*{{u_minus}}[i-1])*_m
+    for _i in range(N):
+        {{u_minus}}[_i] = {{b_minus}}[_i] # RHS -> v_star (solution)
+        bi = {{ab_minus}}[N + _i] - _gtot_all[_i] # main diagonal
+        if (_i<N-1):
+            c[_i] = {{ab_minus}}[0 + _i+1] # superdiagonal
+        if (_i>0):
+            ai = {{ab_minus}}[2*N + _i-1] # subdiagonal
+            _m = 1.0/(bi-ai*c[_i-1])
+            c[_i] = c[_i]*_m
+            {{u_minus}}[_i] = ({{u_minus}}[_i] - ai*{{u_minus}}[_i-1])*_m
         else:
             c[0] = c[0]/bi
             {{u_minus}}[0] = {{u_minus}}[0]/bi
-    for i in range(N-2, 0, -1):
-        {{u_minus}}[i] = {{u_minus}}[i] - c[i]*{{u_minus}}[i+1]
+    for _i in range(N-2, -1, -1):
+        {{u_minus}}[_i] = {{u_minus}}[_i] - c[_i]*{{u_minus}}[_i+1]
 
 {% endblock %}
