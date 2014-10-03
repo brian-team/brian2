9c6df3977b2551921d7df4fe572134d4102e33c9
thesamovar <dan.goodman@ens.fr>
2014-10-03 21:20:59 +0100

    Cleaned up CUBA example - regression on setting by string? [ci skip]

--- examples/cuba.py
+++ examples/cuba.py
@@ -1,8 +1,6 @@
 from brian2 import *
 import time
 
-# BrianLogger.log_level_debug()
-
 taum = 20 * ms
 taue = 5 * ms
 taui = 10 * ms
@@ -17,23 +15,20 @@ dgi/dt = -gi/taui : volt (unless refractory)
 '''
 
 P = NeuronGroup(4000, eqs, threshold='v>Vt', reset='v=Vr', refractory=5 * ms)
-P.v = Vr
+P.v = 'Vr + rand() * (Vt - Vr)'
 P.ge = 0 * mV
 P.gi = 0 * mV
 
 we = (60 * 0.27 / 10) * mV # excitatory synaptic weight (voltage)
 wi = (-20 * 4.5 / 10) * mV # inhibitory synaptic weight
-Ce = Synapses(P, P, 'w:1', pre='ge += we')
-Ci = Synapses(P, P, 'w:1', pre='gi += wi')
+Ce = Synapses(P, P, pre='ge += we')
+Ci = Synapses(P, P, pre='gi += wi')
 Ce.connect('i<3200', p=0.02)
 Ci.connect('i>=3200', p=0.02)
-P.v = Vr + rand(len(P)) * (Vt - Vr)
 
-start_time = time.time()
 s_mon = SpikeMonitor(P)
-run(1 * second)
-duration = time.time() - start_time
 
-print duration
-plot(s_mon.t/ms, s_mon.i, '.')
+run(1 * second)
+
+plot(s_mon.t/ms, s_mon.i, '.k')
 show()
