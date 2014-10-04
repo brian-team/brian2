ab4e3c4570b6f5a16b64850fea8caecf235a67cd
thesamovar <dan.goodman@ens.fr>
2014-10-04 15:22:59 +0100

    Categorise examples

--- brian2/sphinxext/generate_examples.py
+++ brian2/sphinxext/generate_examples.py
@@ -2,6 +2,7 @@ import os
 import fnmatch
 import shutil
 import unicodedata
+from collections import defaultdict
 
 
 class GlobDirectoryWalker:
@@ -31,6 +32,7 @@ class GlobDirectoryWalker:
                 if fnmatch.fnmatch(file, self.pattern):
                     return fullname
 
+
 def main(rootpath, destdir):
     if not os.path.exists(destdir):
         shutil.os.makedirs(destdir)
@@ -39,18 +41,30 @@ def main(rootpath, destdir):
     print 'Documenting %d examples' % len(examplesfnames)
     examplespaths = []
     examplesbasenames = []
+    relativepaths = []
+    outnames = []
     for f in examplesfnames:
         path, file = os.path.split(f)
+        relpath = os.path.relpath(path, rootpath)
+        if relpath=='.':
+            relpath = ''
         path = os.path.normpath(path)
         filebase, ext = os.path.splitext(file)
+        exname = filebase
+        if relpath:
+            exname = relpath.replace('/', '.').replace('\\', '.')+'.'+exname
         examplespaths.append(path)
         examplesbasenames.append(filebase)
+        relativepaths.append(relpath)
+        outnames.append(exname)
     examplescode = [open(fname, 'rU').read() for fname in examplesfnames]
     examplesdocs = []
     examplesafterdoccode = []
     examplesdocumentablenames = []
     for code in examplescode:
         codesplit = code.split('\n')
+        if codesplit[0].startswith('#'):
+            codesplit = codesplit[1:]
         readingdoc = False
         doc = []
         afterdoccode = ''
@@ -71,9 +85,13 @@ def main(rootpath, destdir):
         # next line replaces unicode characters like e-acute with standard ascii representation
         examplesdocs.append(unicodedata.normalize('NFKD', unicode(doc, 'latin-1')).encode('ascii', 'ignore'))
         examplesafterdoccode.append(afterdoccode)
-    
-    examples = zip(examplesfnames, examplespaths, examplesbasenames, examplescode, examplesdocs, examplesafterdoccode)
-    for fname, path, basename, code, docs, afterdoccode in examples:
+        
+    categories = defaultdict(list)    
+    examples = zip(examplesfnames, examplespaths, examplesbasenames,
+                   examplescode, examplesdocs, examplesafterdoccode,
+                   relativepaths, outnames)
+    for fname, path, basename, code, docs, afterdoccode, relpath, exname in examples:
+        categories[relpath].append(exname)
         title = 'Example: ' + basename
         output = '.. currentmodule:: brian2\n\n'
         output += '.. ' + basename + ':\n\n'
@@ -82,14 +100,29 @@ def main(rootpath, destdir):
         output += '\n'.join(['    ' + line for line in afterdoccode.split('\n')])
         output += '\n\n'
     
-        open(os.path.join(destdir, basename + '.rst'), 'w').write(output)
+        open(os.path.join(destdir, exname + '.rst'), 'w').write(output)
     
     mainpage_text =  'Examples\n'
     mainpage_text += '========\n\n'
-    mainpage_text += '.. toctree::\n'
-    mainpage_text += '   :maxdepth: 1\n\n'
-    curpath = ''
-    for fname, path, basename, code, docs, afterdoccode in examples:
-        mainpage_text += '   ' + basename + '\n'
+    
+    def insert_category(category, mainpage_text):
+        if category:
+            mainpage_text += '\n'+category+'\n'+'-'*len(category)+'\n\n'
+        mainpage_text += '.. toctree::\n'
+        mainpage_text += '   :maxdepth: 1\n\n'
+        curpath = ''
+        for exname in sorted(categories[category]):
+            mainpage_text += '   ' + exname + '\n'
+        return mainpage_text
+            
+    mainpage_text = insert_category('', mainpage_text)
+    for category in sorted(categories.keys()):
+        if category:
+            mainpage_text = insert_category(category, mainpage_text)
+            
     open(os.path.join(destdir, 'index.rst'), 'w').write(mainpage_text)
+    
+
+if __name__=='__main__':
+    main('../../examples', '../../docs_sphinx/examples')
     
