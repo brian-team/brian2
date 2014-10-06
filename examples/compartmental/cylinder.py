a68e6f67ca0c77be2f6071aed9275989ad105205
thesamovar <dan.goodman@ens.fr>
2014-10-06 16:35:51 +0100

    Cleaned up compartmental examples

--- examples/compartmental/cylinder.py
+++ examples/compartmental/cylinder.py
@@ -5,42 +5,41 @@ from brian2 import *
 
 defaultclock.dt = 0.01*ms
 
-brian_prefs.codegen.target = 'weave'
-
 # Morphology
 diameter = 1*um
 length = 300*um
-Cm = 1 * uF / cm ** 2
-Ri = 150 * ohm * cm
+Cm = 1*uF/cm**2
+Ri = 150*ohm*cm
 N = 200
-morpho=Cylinder(diameter=diameter,length=length,n=N)
+morpho = Cylinder(diameter=diameter, length=length, n=N)
 
 # Passive channels
-gL=1e-4*siemens/cm**2
-EL=-70*mV
-eqs='''
-Im=gL*(EL-v) : amp/meter**2
+gL = 1e-4*siemens/cm**2
+EL = -70*mV
+eqs = '''
+Im = gL * (EL - v) : amp/meter**2
 I : amp (point current)
 '''
 
 neuron = SpatialNeuron(morphology=morpho, model=eqs, Cm=Cm, Ri=Ri)
 neuron.v = EL
 
-taum = Cm/gL # membrane time constant
-print "Time constant",taum
-rm = 1/(gL * pi * diameter) # membrane resistance per unit length
-ra = (4 * Ri)/(pi * diameter**2) # axial resistance per unit length
-la = sqrt(rm/ra) # space length
-print "Characteristic length",la
+taum = Cm / gL # membrane time constant
+print "Time constant", taum
+rm = 1 / (gL * pi * diameter) # membrane resistance per unit length
+ra = (4 * Ri) / (pi * diameter**2) # axial resistance per unit length
+la = sqrt(rm / ra) # space length
+print "Characteristic length", la
 
-neuron.I[0]=0.02*nA # injecting at the left end
-run(100*ms,report='text')
+neuron.I[0] = 0.02*nA # injecting at the left end
+run(100*ms, report='text')
 
-plot(neuron.distance/um,neuron.v/mV,'k')
+plot(neuron.distance/um, neuron.v/mV, 'k')
 # Theory
 x = neuron.distance
-ra = la*4*Ri/(pi*diameter**2)
-theory = EL+ra*neuron.I[0]*cosh((length-x)/la)/sinh(length/la)
-plot(x/um, theory/mV,'r')
-
+ra = la * 4 * Ri / (pi * diameter**2)
+theory = EL + ra * neuron.I[0] * cosh((length - x) / la) / sinh(length / la)
+plot(x/um, theory/mV, 'r')
+xlabel('x (um)')
+ylabel('v (mV)')
 show()
