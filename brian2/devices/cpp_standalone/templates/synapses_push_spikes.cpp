fd8342ee397311985426b60ca6acedae4dd23f64
Marcel Stimberg <marcel.stimberg@ens.fr>
2014-10-06 12:12:05 +0200

    Fix two regressions in standalone examples introduced during the OpenMP merge: N_incoming and N_outgoing were not updated during synapse creation and the wrong size argument was used in the "push" template when used with subgroups

--- brian2/devices/cpp_standalone/templates/synapses_push_spikes.cpp
+++ brian2/devices/cpp_standalone/templates/synapses_push_spikes.cpp
@@ -23,7 +23,7 @@ void _run_{{codeobj_name}}()
 	// we do advance at the beginning rather than at the end because it saves us making
 	// a copy of the current spiking synapses
 	{{owner.name}}.advance();
-	{{owner.name}}.push({{_spikespace}}, {{_spikespace}}[{{owner.source|length}}]);
+	{{owner.name}}.push({{_spikespace}}, {{_spikespace}}[_num_spikespace-1]);
 	//{{owner.name}}.queue->peek();
 }
 {% endmacro %}
