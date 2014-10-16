04de3d6163749ddd62e7c9ae339669372a9e39d9
Marcel Stimberg <marcel.stimberg@ens.fr>
2014-10-16 14:39:23 +0200

    Make the direct use of `TimedArrays` in Python (e.g. not in code strings) have semantics that are consistent with the interpretation during a simulation run. Closes #345

--- brian2/tests/test_timedarray.py
+++ brian2/tests/test_timedarray.py
@@ -1,9 +1,11 @@
 import numpy as np
 from numpy.testing.utils import assert_equal
+from nose.plugins.attrib import attr
 
 from brian2 import *
 
 
+@attr('codegen-independent')
 def test_timedarray_direct_use():
     ta = TimedArray(np.linspace(0, 10, 11), 1*ms)
     assert ta(-1*ms) == 0
@@ -15,6 +17,18 @@ def test_timedarray_direct_use():
     assert ta(5*ms) == 5*amp
     assert ta(10*ms) == 10*amp
     assert ta(15*ms) == 10*amp
+
+
+def test_timedarray_semantics():
+    # Make sure that timed arrays are interpreted as specifying the values
+    # between t and t+dt (not between t-dt/2 and t+dt/2 as in Brian1)
+    ta = TimedArray(array([0, 1]), dt=0.4*ms)
+    G = NeuronGroup(1, 'value = ta(t) : 1', dt=0.1*ms)
+    mon = StateMonitor(G, 'value', record=0)
+    net = Network(G, mon)
+    net.run(0.8*ms)
+    assert_equal(mon[0].value, [0, 0, 0, 0, 1, 1, 1, 1])
+    assert_equal(mon[0].value, ta(mon.t))
 
 
 def test_timedarray_no_units():
@@ -64,6 +78,7 @@ def test_long_timedarray():
 
 if __name__ == '__main__':
     test_timedarray_direct_use()
+    test_timedarray_semantics()
     test_timedarray_no_units()
     test_timedarray_with_units()
     test_timedarray_no_upsampling()
