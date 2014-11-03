d70043585ded7fad6d3c95d61430ef5b4125d63d
Marcel Stimberg <marcel.stimberg@ens.fr>
2014-11-03 17:21:51 +0100

    For now, evaluate the threshold and SpikeGeneratorGroup in a single thread. The previous solution assumed knowledge about the static scheduling that I don't think we have. Fixes #358
    (cherry picked from commit 089efeecff8f3bb4b9f83853842af2de7fbdd30f)

--- brian2/devices/cpp_standalone/templates/spikegenerator.cpp
+++ brian2/devices/cpp_standalone/templates/spikegenerator.cpp
@@ -4,26 +4,23 @@
 	{# USES_VARIABLES {_spikespace, N, t, dt, neuron_index, spike_time } #}
 
     // TODO: We don't deal with more than one spike per neuron yet
-    long _cpp_numspikes = 0;
-    // Note that std:upper_bound returns a pointer but we want indices
-    const unsigned int _start_idx = std::upper_bound({{spike_time}},
-                                                     {{spike_time}} + _numspike_time,
-                                                     t-dt) - {{spike_time}};
-    const unsigned int _stop_idx = std::upper_bound({{spike_time}},
-                                                    {{spike_time}} + _numspike_time,
-                                                    t) - {{spike_time}};
-    
-    const long _nb_spikes = _stop_idx - _start_idx;
-    const long _padding   = {{ openmp_pragma('get_thread_num') }}*(_nb_spikes/{{ openmp_pragma('get_num_threads') }});
-    long           _count = 0;
+    {{ openmp_pragma('single') }}
+        {
+        // Note that std:upper_bound returns a pointer but we want indices
+        const unsigned int _start_idx = std::upper_bound({{spike_time}},
+                                                         {{spike_time}} + _numspike_time,
+                                                         t-dt) - {{spike_time}};
+        const unsigned int _stop_idx = std::upper_bound({{spike_time}},
+                                                        {{spike_time}} + _numspike_time,
+                                                        t) - {{spike_time}};
 
-    {{ openmp_pragma('static') }}
-    for(int _idx=_start_idx; _idx<_stop_idx; _idx++)
-    {
-        {{_spikespace}}[_padding + _count] = {{neuron_index}}[_idx];
-        _count++;
+        long _count = 0;
+
+        for(int _idx=_start_idx; _idx<_stop_idx; _idx++)
+        {
+            {{_spikespace}}[_count++] = {{neuron_index}}[_idx];
+        }
+
+        {{_spikespace}}[N] = _count;
     }
-
-    {{_spikespace}}[N] = _nb_spikes;
-
 {% endblock %}
