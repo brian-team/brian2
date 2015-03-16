def516e52885b81daa8cfb48c2a7b23d21f78dc1
Marcel Stimberg <marcel.stimberg@inserm.fr>
2015-03-16 17:44:30 +0100

    Add a `PoissonInput` group Closes #221

--- brian2/groups/__init__.py
+++ brian2/groups/__init__.py
@@ -5,5 +5,6 @@ Package providing groups such as `NeuronGroup` or `PoissonGroup`.
 from .group import *
 from .neurongroup import *
 from .poissongroup import *
+from .poisson_input import *
 from .spikegeneratorgroup import *
 from .subgroup import *
