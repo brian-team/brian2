399793ce8a23f408029f5d93232e94cdeb32eed7
Vinay Chandragiri <vinaychandragiri7@gmail.com>
2015-03-25 18:32:54 +0530

    Import and Export - 3

--- brian2/groups/exp_imp.py
+++ brian2/groups/exp_imp.py
@@ -1,25 +1,20 @@
 from brian2.groups.group import Group
 
-class ImportExport(self,object):
-
-	# This class is used to export/import data into other formats
-
-	"""docstring for ImportExport"""
+class ImportExport(object):
+	"""docstring for Impo"""
 	def __init__(self, arg):
-		super(ImportExport, self).__init__()
+		super Impo, self).__init__()
 		self.arg = arg
 
 	@staticmethod
-	def export_func1(self,vars,units = True):
+	def export_func1(vars,units = True):
         data = {}
         for var in vars:
             data[var] = np.array(Group().state(var, use_units=units,level=level+1),copy=True, subok=True)
         return data
 
     @staticmethod
-
-    def export_func2(self ,vars,units = True):
-
+    def export_func2(vars,units = True):
         old_data = {}
         for var in vars:
 
@@ -27,6 +22,8 @@ class ImportExport(self,object):
 
         data = pd.DataFrame(data = old_data); # Mentioning columns here was not necessary
         return data
+		
+
 
 
 		
