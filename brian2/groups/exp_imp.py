a900387cb6806557408f81a29b1052e2fd26ec71
Vinay Chandragiri <vinaychandragiri7@gmail.com>
2015-03-25 18:34:12 +0530

    Import and Export - 3

--- brian2/groups/exp_imp.py
+++ brian2/groups/exp_imp.py
@@ -1,27 +1,28 @@
 from brian2.groups.group import Group
 
-class ImportExport(object):
-	"""docstring for Impo"""
-	def __init__(self, arg):
-		super Impo, self).__init__()
-		self.arg = arg
+	class ImportExport(object):
+		"""docstring for Impo"""
+		def __init__(self, arg):
 
-	@staticmethod
-	def export_func1(vars,units = True):
-        data = {}
-        for var in vars:
-            data[var] = np.array(Group().state(var, use_units=units,level=level+1),copy=True, subok=True)
-        return data
+			super Impo, self).__init__()
+			self.arg = arg
 
-    @staticmethod
-    def export_func2(vars,units = True):
-        old_data = {}
-        for var in vars:
+		@staticmethod
+		def export_func1(vars,units = True):
+        	data = {}
+        	for var in vars:
+            	data[var] = np.array(Group().state(var, use_units=units,level=level+1),copy=True, subok=True)
+        	return data
 
-            old_data[var] = np.array(Group().state(var, use_units=units,level=level+1),copy=True, subok=True)
+	    @staticmethod
+	    def export_func2(vars,units = True):
+	        old_data = {}
+	        for var in vars:
 
-        data = pd.DataFrame(data = old_data); # Mentioning columns here was not necessary
-        return data
+	            old_data[var] = np.array(Group().state(var, use_units=units,level=level+1),copy=True, subok=True)
+
+	        data = pd.DataFrame(data = old_data); # Mentioning columns here was not necessary
+	        return data
 		
 
 
