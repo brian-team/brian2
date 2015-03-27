821a5a7486e71dd463632d505f9205a8cf2f2210
Marcel Stimberg <marcel.stimberg@inserm.fr>
2015-03-27 17:35:56 +0100

    Use static arrays/structs instead of dynamic memory allocation with malloc

--- brian2/devices/c_standalone/templates/objects.c
+++ brian2/devices/c_standalone/templates/objects.c
@@ -8,17 +8,19 @@
 #include<stdio.h>
 
 {% for clock in clocks | sort(attribute='name') %}
-Clock *{{clock.name}};
+Clock _{{clock.name}};
+Clock *{{clock.name}} = &_{{clock.name}};
 {% endfor %}
 
 {% for net in networks | sort(attribute='name') %}
-Network *{{net.name}};
+Network _{{net.name}};
+Network *{{net.name}} = &_{{net.name}};
 {% endfor %}
 
 //////////////// arrays ///////////////////
 {% for var, varname in array_specs | dictsort(by='value') %}
 {% if not var in dynamic_array_specs %}
-{{c_data_type(var.dtype)}} * {{varname}};
+{{c_data_type(var.dtype)}} {{varname}}[{{var.length}}] = {0,};
 const int _num_{{varname}} = {{var.length}};
 {% endif %}
 {% endfor %}
@@ -27,7 +29,7 @@ const int _num_{{varname}} = {{var.length}};
 {% for (name, dtype_spec, N, filename) in static_array_specs | sort %}
 {# arrays that are initialized from static data are already declared #}
 {% if not name in array_specs.values() %}
-{{dtype_spec}} * {{name}};
+{{dtype_spec}} {{name}}[{{N}}];
 const int _num_{{name}} = {{N}};
 {% endif %}
 {% endfor %}
@@ -36,32 +38,14 @@ void _init_arrays()
 {
     //////////////// clocks ///////////////////
     {% for clock in clocks | sort(attribute='name') %}
-    {{clock.name}} = malloc(sizeof(Clock));
     Clock_construct({{clock.name}}, {{clock.dt_}}, 1e-14);
-    {% endfor %}
-
-    //////////////// networks /////////////////
-    {% for net in networks | sort(attribute='name') %}
-    {{net.name}} = malloc(sizeof(Network));
-    {% endfor %}
-
-    // Arrays initialized to 0
-	{% for var in zero_arrays | sort(attribute='name') %}
-	{% set varname = array_specs[var] %}
-	{{varname}} = calloc(sizeof({{c_data_type(var.dtype)}}), {{var.length}});
     {% endfor %}
 
 	// Arrays initialized to an "arange"
 	{% for var, start in arange_arrays %}
 	{% set varname = array_specs[var] %}
-	{{varname}} = malloc(sizeof({{c_data_type(var.dtype)}}) * {{var.length}});
 	{{ openmp_pragma('parallel-static') }}
 	for(int i=0; i<{{var.length}}; i++) {{varname}}[i] = {{start}} + i;
-	{% endfor %}
-
-	// static arrays
-	{% for (name, dtype_spec, N, filename) in static_array_specs | sort %}
-	{{name}} = malloc(sizeof({{dtype_spec}}) * {{N}});
 	{% endfor %}
 }
 
@@ -110,7 +94,6 @@ extern Clock* {{clock.name}};
 {% endfor %}
 
 //////////////// networks /////////////////
-extern Network* magicnetwork;
 {% for net in networks %}
 extern Network* {{net.name}};
 {% endfor %}
@@ -118,7 +101,7 @@ extern Network* {{net.name}};
 
 //////////////// arrays ///////////////////
 {% for var, varname in array_specs | dictsort(by='value') %}
-extern {{c_data_type(var.dtype)}} *{{varname}};
+extern {{c_data_type(var.dtype)}} {{varname}}[];
 extern const int _num_{{varname}};
 {% endfor %}
 
@@ -127,7 +110,7 @@ extern const int _num_{{varname}};
 {% for (name, dtype_spec, N, filename) in static_array_specs | sort %}
 {# arrays that are initialized from static data are already declared #}
 {% if not name in array_specs.values() %}
-extern {{dtype_spec}} *{{name}};
+extern {{dtype_spec}} {{name}}[];
 extern const int _num_{{name}};
 {% endif %}
 {% endfor %}
