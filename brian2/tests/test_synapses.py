08e23a165157a2d14d3f5437426c9ab4cd5a38dc
Marcel Stimberg <marcel.stimberg@inserm.fr>
2015-06-19 13:50:23 +0200

    Fix synapses tests: use a threshold for all groups that are connected to a synaptic pathway

--- brian2/tests/test_synapses.py
+++ brian2/tests/test_synapses.py
@@ -32,7 +32,7 @@ def test_creation():
     '''
     A basic test that creating a Synapses object works.
     '''
-    G = NeuronGroup(42, 'v: 1')
+    G = NeuronGroup(42, 'v: 1', threshold='False')
     S = Synapses(G, G, 'w:1', pre='v+=w')
     # We store weakref proxys, so we can't directly compare the objects
     assert S.source.name == S.target.name == G.name
@@ -62,8 +62,8 @@ def test_incoming_outgoing():
     (It will be also automatically tested for all connection patterns that
     use the above _compare function for testing)
     '''
-    G1 = NeuronGroup(5, 'v: 1')
-    G2 = NeuronGroup(5, 'v: 1')
+    G1 = NeuronGroup(5, 'v: 1', threshold='False')
+    G2 = NeuronGroup(5, 'v: 1', threshold='False')
     S = Synapses(G1, G2, 'w:1', pre='v+=w')
     S.connect([0, 0, 0, 1, 1, 2],
               [0, 1, 2, 1, 2, 3])
@@ -175,9 +175,9 @@ def test_connection_string_deterministic():
     '''
     Test connecting synapses with a deterministic string expression.
     '''
-    G = NeuronGroup(17, 'v : 1')
+    G = NeuronGroup(17, 'v : 1', threshold='False')
     G.v = 'i'
-    G2 = NeuronGroup(4, 'v : 1')
+    G2 = NeuronGroup(4, 'v : 1', threshold='False')
     G2.v = '17 + i'
 
     # Full connection
@@ -255,8 +255,8 @@ def test_connection_random():
     '''
     Test random connections.
     '''
-    G = NeuronGroup(4, 'v: 1')
-    G2 = NeuronGroup(7, 'v: 1')
+    G = NeuronGroup(4, 'v: 1', threshold='False')
+    G2 = NeuronGroup(7, 'v: 1', threshold='False')
     # We can only test probabilities 0 and 1 for strict correctness
     S = Synapses(G, G2, 'w:1', 'v+=w')
     S.connect('rand() < 0.')
@@ -313,8 +313,8 @@ def test_connection_multiple_synapses():
     '''
     Test multiple synapses per connection.
     '''
-    G = NeuronGroup(42, 'v: 1')
-    G2 = NeuronGroup(17, 'v: 1')
+    G = NeuronGroup(42, 'v: 1', threshold='False')
+    G2 = NeuronGroup(17, 'v: 1', threshold='False')
 
     S = Synapses(G, G2, 'w:1', 'v+=w')
     S.connect(True, n=0)
@@ -334,7 +334,7 @@ def test_state_variable_assignment():
     Assign values to state variables in various ways
     '''
 
-    G = NeuronGroup(10, 'v: volt')
+    G = NeuronGroup(10, 'v: volt', threshold='False')
     G.v = 'i*mV'
     S = Synapses(G, G, 'w:volt')
     S.connect(True)
@@ -466,7 +466,7 @@ def test_subexpression_references():
 def test_delay_specification():
     # By default delays are state variables (i.e. arrays), but if they are
     # specified in the initializer, they are scalars.
-    G = NeuronGroup(10, 'v:1')
+    G = NeuronGroup(10, 'v:1', threshold='False')
 
     # Array delay
     S = Synapses(G, G, 'w:1', pre='v+=w')
@@ -583,8 +583,8 @@ def test_clocks():
     source_dt = 0.05*ms
     target_dt = 0.1*ms
     synapse_dt = 0.2*ms
-    source = NeuronGroup(1, 'v:1', dt=source_dt)
-    target = NeuronGroup(1, 'v:1', dt=target_dt)
+    source = NeuronGroup(1, 'v:1', dt=source_dt, threshold='False')
+    target = NeuronGroup(1, 'v:1', dt=target_dt, threshold='False')
     synapse = Synapses(source, target, 'w:1', pre='v+=1', post='v+=1',
                        dt=synapse_dt, connect=True)
 
@@ -682,7 +682,7 @@ def test_summed_variable_errors():
 
 def test_scalar_parameter_access():
     G = NeuronGroup(10, '''v : 1
-                           scalar : Hz (shared)''')
+                           scalar : Hz (shared)''', threshold='False')
     S = Synapses(G, G, '''w : 1
                           s : Hz (shared)
                           number : 1 (shared)''',
@@ -721,7 +721,7 @@ def test_scalar_parameter_access():
 
 def test_scalar_subexpression():
     G = NeuronGroup(10, '''v : 1
-                           number : 1 (shared)''')
+                           number : 1 (shared)''', threshold='False')
     S = Synapses(G, G, '''s : 1 (shared)
                           sub = number_post + s : 1 (shared)''',
                  pre='v+=s', connect=True)
@@ -800,7 +800,7 @@ def test_event_driven():
 
 @attr('codegen-independent')
 def test_repr():
-    G = NeuronGroup(1, 'v: volt')
+    G = NeuronGroup(1, 'v: volt', threshold='False')
     S = Synapses(G, G,
                  '''w : 1
                     dApre/dt = -Apre/taupre : 1 (event-driven)
