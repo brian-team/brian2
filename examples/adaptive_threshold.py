c1841e8b73ea92815a3aac9df7096c6c5a017c42
Marcel Stimberg <marcel.stimberg@inserm.fr>
2015-07-07 15:52:18 +0200

    Add an example that uses the new "record other variables" feature of `SpikeMonitor`

---
+++ examples/adaptive_threshold.py
@@ -0,0 +1,39 @@
+from brian2 import *
+'''
+A model with adaptive threshold (increases with each spike)
+'''
+#set_device('cpp_standalone_simple')
+eqs = '''
+dv/dt = -v/(10*ms) : volt
+dvt/dt = (10*mV-vt)/(15*ms) : volt
+'''
+
+reset = '''
+v = 0*mV
+vt += 3*mV
+'''
+
+IF = NeuronGroup(1, model=eqs, reset=reset, threshold='v>vt')
+IF.vt = 10*mV
+PG = PoissonGroup(1, 500 * Hz)
+
+C = Synapses(PG, IF, pre='v += 3*mV', connect=True)
+
+Mv = StateMonitor(IF, 'v', record=True)
+Mvt = StateMonitor(IF, 'vt', record=True)
+# Record the value of v when the threshold is crossed
+M_crossings = SpikeMonitor(IF, variables='v')
+run(2*second, report='text')
+# print M_crossings.codeobj.code
+
+subplot(1, 2, 1)
+plot(Mv.t / ms, Mv[0].v / mV)
+plot(Mvt.t / ms, Mvt[0].vt / mV)
+ylabel('v (mV)')
+xlabel('t (ms)')
+# zoom in on the first 100ms
+xlim(0, 100)
+subplot(1, 2, 2)
+hist(M_crossings.v / mV, bins=np.arange(10, 20, 0.5))
+xlabel('v at threshold crossing (mV)')
+show()
