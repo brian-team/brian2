24c38a26a04170dc90d0466f96a998f470bcf837
thesamovar <dg.github@thesamovar.net>
2015-09-15 18:26:57 +0100

    Profile push_spikes in standalone

--- brian2/devices/cpp_standalone/templates/synapses_push_spikes.cpp
+++ brian2/devices/cpp_standalone/templates/synapses_push_spikes.cpp
@@ -6,10 +6,18 @@
 #include "brianlib/common_math.h"
 #include "brianlib/stdint_compat.h"
 #include<cmath>
+#include<ctime>
 
 void _run_{{codeobj_name}}()
 {
 	using namespace brian;
+
+    {% if openmp_pragma('with_openmp') %}
+    const double _start_time = omp_get_wtime();
+    {% else %}
+    const std::clock_t _start_time = std::clock();
+    {% endif %}
+
     ///// CONSTANTS ///////////
 	%CONSTANTS%
 	///// POINTERS ////////////
@@ -25,6 +33,14 @@ void _run_{{codeobj_name}}()
         {{owner.name}}.advance();
         {{owner.name}}.push({{_eventspace}}, {{_eventspace}}[_num{{eventspace_variable.name}}-1]);
 	}
+
+	// Profiling
+    {% if openmp_pragma('with_openmp') %}
+    const double _run_time = omp_get_wtime() -_start_time;
+    {% else %}
+    const double _run_time = (double)(std::clock() -_start_time)/CLOCKS_PER_SEC;
+    {% endif %}
+    {{codeobj_name}}_profiling_info += _run_time;
 }
 {% endmacro %}
 
