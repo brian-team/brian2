4fe038cede9a8525e3db8f4ac0bd3c381fa7b7c1
Marcel Stimberg <marcel.stimberg@inserm.fr>
2015-10-18 18:07:39 +0200

    Fix the `summed_variable` template for OpenMP

--- brian2/devices/cpp_standalone/templates/summed_variable.cpp
+++ brian2/devices/cpp_standalone/templates/summed_variable.cpp
@@ -4,12 +4,10 @@
     {# USES_VARIABLES { _synaptic_post, _synaptic_pre, N_post } #}
     {% set _target_var_array = get_array_name(_target_var) %}
     //// MAIN CODE ////////////
-    // Set all the target variable values to zero
-    std::vector<{{c_data_type(_target_var.dtype)}}> _local_sum;
     {# This enables summed variables for connections to a synapse #}
     const int _N_post = {{constant_or_scalar('N_post', variables['N_post'])}};
-    _local_sum.resize(_N_post, 0);
 
+    // Set all the target variable values to zero
     {{ openmp_pragma('parallel-static') }}
     for (int _target_idx=0; _target_idx<_N_post; _target_idx++)
     {
@@ -26,13 +24,7 @@
         // vector code
         const int _vectorisation_idx = _idx;
         {{vector_code|autoindent}}
-        _local_sum[{{_synaptic_post}}[_idx]] += _synaptic_var;
-    }
-
-    {{ openmp_pragma('parallel-static') }}
-    for (int _target_idx=0; _target_idx<_N_post; _target_idx++)
-    {
         {{ openmp_pragma('atomic') }}
-        {{_target_var_array}}[_target_idx] += _local_sum[_target_idx];
+        {{_target_var_array}}[{{_synaptic_post}}[_idx]] += _synaptic_var;
     }
 {% endblock %}
