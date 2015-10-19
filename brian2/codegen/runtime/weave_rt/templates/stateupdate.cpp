b3ab06627680f58ca9267d80aad602e28d0cc495
Marcel Stimberg <marcel.stimberg@inserm.fr>
2015-10-19 14:34:47 +0200

    Fix a few weave templates to get the number of neurons/synapses in the correct way

--- brian2/codegen/runtime/weave_rt/templates/stateupdate.cpp
+++ brian2/codegen/runtime/weave_rt/templates/stateupdate.cpp
@@ -5,11 +5,14 @@
 {% block maincode %}
 	//// MAIN CODE ////////////
 
+    // This allows everything to work correctly for synapses where N is not a
+    // constant
+    const int _N = {{constant_or_scalar('N', variables['N'])}};
 	// scalar code
 	const int _vectorisation_idx = 1;
 	{{scalar_code|autoindent}}
 
-	for(int _idx=0; _idx<N; _idx++)
+	for(int _idx=0; _idx<_N; _idx++)
 	{
 	    // vector code
 		const int _vectorisation_idx = _idx;
