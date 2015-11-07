6265452ad75d052e0f91492e52668c9f15f6f345
thesamovar <dg.github@thesamovar.net>
2015-11-07 14:42:20 +0000

    Arithmetic simplifications done [ci skip]

--- brian2/codegen/optimisation.py
+++ brian2/codegen/optimisation.py
@@ -39,6 +39,41 @@ class Simplifier(BrianASTRenderer):
                     # only simplify this if the type wouldn't be cast by the operation
                     if dtype_hierarchy[right.dtype]<=dtype_hierarchy[left.dtype]:
                         return left
+        # Handle division by 1, or 0/x
+        if op.__class__.__name__=='Div':
+            if left.__class__.__name__=='Num':
+                if left.n==0:
+                    # must not change the dtype of the output, e.g. handle 0/float->0.0 and 0.0/int->0.0
+                    left.dtype = node.dtype
+                    if node.dtype=='integer':
+                        left.n = 0
+                    else:
+                        left.n = 0.0
+                    return left
+            if right.__class__.__name__=='Num':
+                if right.n==1:
+                    # only simplify this if the type wouldn't be cast by the operation
+                    if dtype_hierarchy[right.dtype]<=dtype_hierarchy[left.dtype]:
+                        return left
+        # Handle addition of 0
+        if op.__class__.__name__=='Add':
+            if left.__class__.__name__=='Num':
+                if left.n==0:
+                    # only simplify this if the type wouldn't be cast by the operation
+                    if dtype_hierarchy[left.dtype]<=dtype_hierarchy[right.dtype]:
+                        return right
+            if right.__class__.__name__=='Num':
+                if right.n==0:
+                    # only simplify this if the type wouldn't be cast by the operation
+                    if dtype_hierarchy[right.dtype]<=dtype_hierarchy[left.dtype]:
+                        return left
+        # Handle subtraction of 0
+        if op.__class__.__name__=='Sub':
+            if right.__class__.__name__=='Num':
+                if right.n==0:
+                    # only simplify this if the type wouldn't be cast by the operation
+                    if dtype_hierarchy[right.dtype]<=dtype_hierarchy[left.dtype]:
+                        return left
         return node
 
 if __name__=='__main__':
@@ -50,7 +85,7 @@ if __name__=='__main__':
     b : boolean
     c : integer (shared)
     '''
-    expr = '0.0*a'
+    expr = 'x/1.0'
 
     G = brian2.NeuronGroup(2, eqs)
     variables = {}
