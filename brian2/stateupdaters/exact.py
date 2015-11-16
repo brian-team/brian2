4568acca8846536c20ba1e6f3c114d8f8bd71351
Marcel Stimberg <marcel.stimberg@inserm.fr>
2015-11-16 14:43:21 +0100

    Cleanup after merge of PR #593: remove unused `can_integrate` methods and remove them from the documentation

--- brian2/stateupdaters/exact.py
+++ brian2/stateupdaters/exact.py
@@ -178,24 +178,7 @@ class LinearStateUpdater(StateUpdateMethod):
     A state updater for linear equations. Derives a state updater step from the
     analytical solution given by sympy. Uses the matrix exponential (which is
     only implemented for diagonalizable matrices in sympy).
-    ''' 
-    def can_integrate(self, equations, variables):
-        if equations.is_stochastic:
-            return False
-
-        # Not very efficient but guaranteed to give the correct answer:
-        # Just try to apply the integration method
-        try:
-            # Don't use sympy's simplify because it might take a very long time.
-            # We are not using the result, anyway.
-            self.__call__(equations, variables, simplify=False)
-        except (ValueError, NotImplementedError, TypeError) as ex:
-            logger.debug('Cannot use linear integration: %s' % ex)
-            return False
-
-        # It worked
-        return True
-
+    '''
     def __call__(self, equations, variables=None, simplify=True):
         if equations.is_stochastic:
             raise UnsupportedEquationsException('Cannot solve stochastic '
