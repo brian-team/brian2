f1f0c85daeb4224b182b3c0a3e239c4b3dc0a247
Dr Romain D Cazé <r.caze@imperial.ac.uk>
2016-01-26 17:42:15 +0000

    Update spatialneuron.py
    Adding the event keyword argument

--- brian2/spatialneuron/spatialneuron.py
+++ brian2/spatialneuron/spatialneuron.py
@@ -90,7 +90,7 @@ class SpatialNeuron(NeuronGroup):
     '''
 
     def __init__(self, morphology=None, model=None, threshold=None,
-                 refractory=False, reset=None,
+                 refractory=False, reset=None, event=None,
                  threshold_location=None,
                  dt=None, clock=None, order=0, Cm=0.9 * uF / cm ** 2, Ri=150 * ohm * cm,
                  name='spatialneuron*', dtype=None, namespace=None,
@@ -223,6 +223,7 @@ class SpatialNeuron(NeuronGroup):
 
         NeuronGroup.__init__(self, len(morphology), model=model + eqs_constants,
                              threshold=threshold, refractory=refractory,
+                             event=event,
                              reset=reset,
                              method=method, dt=dt, clock=clock, order=order,
                              namespace=namespace, dtype=dtype, name=name)
