8b7fb564232f6cc5b7e7cbcc35c3342e3d666e99
Marcel Stimberg <marcel.stimberg@inserm.fr>
2016-02-09 18:57:48 +0100

    Try to put all the automatic test logic into a single file, and split a long-running travis run into two

--- dev/tools/run_nose_tests.py
+++ dev/tools/run_nose_tests.py
@@ -6,12 +6,5 @@ import sys
 import brian2
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1 and sys.argv[1] == 'no-parallel':
-        if not brian2.test(test_in_parallel=[]):  # If the test fails, exit with a non-zero error code
-            sys.exit(1)
-    elif len(sys.argv) > 1 and sys.argv[1] == 'cross-compiled':
-        if not brian2.test(reset_preferences=False):  # If the test fails, exit with a non-zero error code
-            sys.exit(1)
-    else:
-        if not brian2.test():  # If the test fails, exit with a non-zero error code
-            sys.exit(1)
+    if not brian2.test():  # If the test fails, exit with a non-zero error code
+        sys.exit(1)
