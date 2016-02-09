0c78c48c60d9cc1947c4e73acccd3a80e476706a
Marcel Stimberg <marcel.stimberg@inserm.fr>
2016-02-09 18:57:48 +0100

    Try to put all the automatic test logic into a single file, and split a long-running travis run into two

---
+++ dev/continuous-integration/run_test_suite.py
@@ -0,0 +1,54 @@
+'''
+Script to run the test suite during automatic testing (easier than putting all
+the logic into Windows batch/bash statements)
+'''
+
+import os
+import sys
+
+import brian2
+
+split_run = os.environ.get('SPLIT_RUN', None)
+standalone = os.environ.get('STANDALONE', 'FALSE').lower() == 'true'
+# If TRAVIS_OS_NAME is not defined, we are testing on appveyor
+os = os.environ.get('TRAVIS_OS_NAME', 'windows').lower()
+cross_compiled = os.environ.get('CROSS_COMPILED', 'FALSE').lower() == 'true'
+report_coverage = os.environ.get('REPORT_COVERAGE', 'FALSE').lower() == 'true'
+
+if split_run == '1':
+    targets = ['numpy', 'weave']
+    independent = True
+elif split_run == '2':
+    targets = ['cython']
+    independent = False
+else:
+    targets = None
+    independent = True
+
+if os == 'windows' or report_coverage:
+    in_parallel = []
+else:
+    in_parallel = ['codegen_independent', 'numpy', 'cython', 'cpp_standalone']
+
+if os in ['linux', 'windows']:
+    openmp = True
+else:
+    openmp = False
+
+reset_preferences = not cross_compiled
+if standalone:
+    result = brian2.test([],
+                         test_codegen_independent=False,
+                         test_standalone='cpp_standalone',
+                         test_openmp=openmp,
+                         test_in_parallel=in_parallel,
+                         reset_preferences=reset_preferences)
+else:
+    result = brian2.test(targets,
+                         test_codegen_independent=independent,
+                         test_standalone=None,
+                         test_in_parallel=in_parallel,
+                         reset_preferences=reset_preferences)
+
+if not result:
+    sys.exit(1)
