1a6e5fe95cbf14009dcdc308e69ab1896b3e0271
Marcel Stimberg <marcel.stimberg@inserm.fr>
2016-04-19 14:40:55 +0200

    Understand  both "true" and "yes" as a value for environment variables in appveyor/travis  In fact, appveyor did not run the standalone test suite because of this problem...

--- dev/continuous-integration/run_test_suite.py
+++ dev/continuous-integration/run_test_suite.py
@@ -9,11 +9,11 @@ import sys
 import brian2
 
 split_run = os.environ.get('SPLIT_RUN', None)
-standalone = os.environ.get('STANDALONE', 'no').lower() == 'yes'
+standalone = os.environ.get('STANDALONE', 'no').lower() in ['yes', 'true']
 # If TRAVIS_OS_NAME is not defined, we are testing on appveyor
 operating_system = os.environ.get('TRAVIS_OS_NAME', 'windows').lower()
-cross_compiled = os.environ.get('CROSS_COMPILED', 'FALSE').lower() == 'true'
-report_coverage = os.environ.get('REPORT_COVERAGE', 'no').lower() == 'yes'
+cross_compiled = os.environ.get('CROSS_COMPILED', 'FALSE').lower() in ['yes', 'true']
+report_coverage = os.environ.get('REPORT_COVERAGE', 'no').lower() in ['yes', 'true']
 
 if split_run == '1':
     targets = ['numpy', 'weave']
