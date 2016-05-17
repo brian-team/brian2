Small refactoring of the unit name system to avoid display issues for compound units involving `Unit(1)` Fixes #696

brian2/tests/test_units.py
@@ -192,7 +192,8 @@ def test_str_repr():
                      array([1, 2, 3]) * kmetre / second,
                      np.ones(3) * nS / cm**2,
                      Unit(1, dim=get_or_create_dimension(length=5, time=2)),
-                     8000*umetre**3, [0.0001, 10000] * umetre**3]
+                     8000*umetre**3, [0.0001, 10000] * umetre**3,
+                     1/metre, 1/(coulomb*metre**2), Unit(1)/second]
     
     unitless = [second/second, 5 * second/second, Unit(1)]
     

brian2/units/fundamentalunits.py
@@ -1796,8 +1796,8 @@ class Unit(Quantity):
     set_display_name
 
     '''
-    __slots__ = ["dim", "scale", "scalefactor", "dispname", "name", "latexname",
-                 "iscompound"]
+    __slots__ = ["dim", "scale", "scalefactor", "_dispname", "_name",
+                 "_latexname", "iscompound"]
 
     __array_priority__ = 100
 
@@ -1817,9 +1817,10 @@ class Unit(Quantity):
         self.dim = getattr(orig, 'dim', DIMENSIONLESS)
         self.scale = getattr(orig, 'scale', ("", "", "", "", "", "", ""))
         self.scalefactor = getattr(orig, 'scalefactor', '')
-        self.name = getattr(orig, 'name', '')
-        self.dispname = getattr(orig, 'dispname', '')
-        self.iscompound = getattr(orig, 'iscompound', False)
+        self._name = getattr(orig, '_name', '')
+        self._dispname = getattr(orig, '_dispname', '')
+        self._latexname = getattr(orig, '_latexname', '')
+        self.iscompound = getattr(orig, '_iscompound', False)
         return self
 
     def __init__(self, value, dim=None, scale=None):
@@ -1836,11 +1837,11 @@ class Unit(Quantity):
         #: The scalefactor for this unit, e.g. 'm' for milli
         self.scalefactor = ""
         #: The full name of this unit.
-        self.name = ""
+        self._name = ""
         #: The display name of this unit.
-        self.dispname = ""
+        self._dispname = ""
         #: A LaTeX expression for the name of this unit.
-        self.latexname = ""
+        self._latexname = ""
         #: Whether this unit is a combination of other units.
         self.iscompound = False
 
@@ -1882,11 +1883,11 @@ class Unit(Quantity):
                 v *= _siprefixes[s] ** i
         u = Unit(v * _siprefixes[scalefactor], dim=dim, scale=tuple(scale))
         u.scalefactor = scalefactor + ""
-        u.name = str(name)
-        u.dispname = str(dispname)
+        u._name = str(name)
+        u._dispname = str(dispname)
         if latexname is None:
-            latexname = u.dispname
-        u.latexname = r'\mathrm{' + latexname + '}'
+            latexname = u._dispname
+        u._latexname = r'\mathrm{' + latexname + '}'
         u.iscompound = False
         return u
 
@@ -1911,8 +1912,8 @@ class Unit(Quantity):
         u = Unit(np.array(baseunit, copy=False) * _siprefixes[scalefactor],
                  dim=baseunit.dim, scale=baseunit.scale)
         u.scalefactor = scalefactor
-        u.name = scalefactor + baseunit.name
-        u.dispname = scalefactor + baseunit.dispname
+        u._name = scalefactor + baseunit._name
+        u._dispname = scalefactor + baseunit._dispname
         # As u --> \mu is the only transformation we have, I think it
         # makes sense to just special-case it here instead of coming
         # up with a general system for scale factors
@@ -1920,24 +1921,13 @@ class Unit(Quantity):
         #it should be an upright letter :-/
         if scalefactor == 'u':
             scalefactor = r'\mu'
-        u.latexname = r'\mathrm{' + scalefactor + '}' + r'\,' + baseunit.latexname
+        u._latexname = r'\mathrm{' + scalefactor + '}' + r'\,' + baseunit.latexname
         u.iscompound = False
         return u
 
     #### METHODS ####
-    def set_name(self, name):
-        """Sets the name for the unit
-        """
-        self.name = name
-
-    def set_display_name(self, name):
-        """Sets the display name for the unit
-        """
-        self.dispname = name
-
-    #### REPRESENTATION ####
-    def __repr__(self):
-        if self.name == "":
+    def get_name(self):
+        if self._name == "":
             if self.scalefactor:
                 parts = [repr(_siprefixes[self.scalefactor])]
             else:
@@ -1955,10 +1945,15 @@ class Unit(Quantity):
             else:
                 return s
         else:
-            return self.name
+            return self._name
 
-    def __str__(self):
-        if self.dispname == "":
+    def set_name(self, name):
+        """Sets the name for the unit
+        """
+        self._name = name
+
+    def get_display_name(self):
+        if self._dispname == "":
             s = self.scalefactor + " "
             for i in range(7):
                 if self.dim._dims[i]:
@@ -1972,10 +1967,15 @@ class Unit(Quantity):
             else:
                 return s
         else:
-            return self.dispname
+            return self._dispname
 
-    def _latex(self, *args):
-        if self.latexname == "":
+    def set_display_name(self, name):
+        """Sets the display name for the unit
+        """
+        self._dispname = name
+
+    def get_latex_name(self):
+        if self._latexname == "":
             if len(self.scalefactor):
                 if self.scalefactor == 'u':
                     scalefactor = r'\mu'
@@ -1996,7 +1996,29 @@ class Unit(Quantity):
             else:
                 return s
         else:
-            return self.latexname
+            return self._latexname
+
+    def set_latex_name(self, name):
+        self._latexname = name
+
+    name = property(fget=get_name, fset=set_name,
+                    doc='The name of the unit')
+
+    dispname = property(fget=get_display_name, fset=set_display_name,
+                        doc='The display name of the unit')
+
+    latexname = property(fget=get_latex_name, fset=set_latex_name,
+                         doc='The LaTeX name of the unit')
+
+    #### REPRESENTATION ####
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.dispname
+
+    def _latex(self, *args):
+        return self.latexname
 
     def _repr_latex_(self):
         return '$' + latex(self) + '$'