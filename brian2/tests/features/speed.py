6bf2554b5e0813dbca1dfce0a2449deecf4d6f84
Marcel Stimberg <marcel.stimberg@inserm.fr>
2016-08-31 17:41:18 +0200

    Add COBAHH and STDP to the speed tests [ci skip]

--- brian2/tests/features/speed.py
+++ brian2/tests/features/speed.py
@@ -7,11 +7,13 @@ from brian2.tests.features import SpeedTest
 __all__ = ['LinearNeuronsOnly',
            'HHNeuronsOnly',
            'CUBAFixedConnectivity',
+           'COBAHHFixedConnectivity',
            'VerySparseMediumRateSynapsesOnly',
            'SparseMediumRateSynapsesOnly',
            'DenseMediumRateSynapsesOnly',
            'SparseLowRateSynapsesOnly',
            'SparseHighRateSynapsesOnly',
+           'STDP'
            ]
 
 
@@ -120,6 +122,131 @@ class CUBAFixedConnectivity(SpeedTest):
         Ci.connect('i>=Ne', p=80. / N)
 
         s_mon = SpikeMonitor(P)
+
+        self.timed_run(self.duration)
+
+
+class COBAHHFixedConnectivity(SpeedTest):
+
+    category = "Full examples"
+    name = "COBAHH fixed connectivity"
+    tags = ["Neurons", "Synapses", "SpikeMonitor"]
+    n_range = [100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000]
+    n_label = 'Num neurons'
+
+    # configuration options
+    duration = 1 * second
+
+    def run(self):
+        N = self.n
+        area = 20000 * umetre ** 2
+        Cm = (1 * ufarad * cm ** -2) * area
+        gl = (5e-5 * siemens * cm ** -2) * area
+
+        El = -60 * mV
+        EK = -90 * mV
+        ENa = 50 * mV
+        g_na = (100 * msiemens * cm ** -2) * area
+        g_kd = (30 * msiemens * cm ** -2) * area
+        VT = -63 * mV
+        # Time constants
+        taue = 5 * ms
+        taui = 10 * ms
+        # Reversal potentials
+        Ee = 0 * mV
+        Ei = -80 * mV
+        we = 6 * nS  # excitatory synaptic weight
+        wi = 67 * nS  # inhibitory synaptic weight
+
+        # The model
+        eqs = Equations('''
+        dv/dt = (gl*(El-v)+ge*(Ee-v)+gi*(Ei-v)-
+                 g_na*(m*m*m)*h*(v-ENa)-
+                 g_kd*(n*n*n*n)*(v-EK))/Cm : volt
+        dm/dt = alpha_m*(1-m)-beta_m*m : 1
+        dn/dt = alpha_n*(1-n)-beta_n*n : 1
+        dh/dt = alpha_h*(1-h)-beta_h*h : 1
+        dge/dt = -ge*(1./taue) : siemens
+        dgi/dt = -gi*(1./taui) : siemens
+        alpha_m = 0.32*(mV**-1)*(13*mV-v+VT)/
+                 (exp((13*mV-v+VT)/(4*mV))-1.)/ms : Hz
+        beta_m = 0.28*(mV**-1)*(v-VT-40*mV)/
+                (exp((v-VT-40*mV)/(5*mV))-1)/ms : Hz
+        alpha_h = 0.128*exp((17*mV-v+VT)/(18*mV))/ms : Hz
+        beta_h = 4./(1+exp((40*mV-v+VT)/(5*mV)))/ms : Hz
+        alpha_n = 0.032*(mV**-1)*(15*mV-v+VT)/
+                 (exp((15*mV-v+VT)/(5*mV))-1.)/ms : Hz
+        beta_n = .5*exp((10*mV-v+VT)/(40*mV))/ms : Hz
+        ''')
+
+        P = NeuronGroup(N, model=eqs, threshold='v>-20*mV',
+                        refractory=3 * ms,
+                        method='exponential_euler')
+        P.v = 'El + (randn() * 5 - 5)*mV'
+        P.ge = '(randn() * 1.5 + 4) * 10.*nS'
+        P.gi = '(randn() * 12 + 20) * 10.*nS'
+
+        Pe = P[:int(0.8*N)]
+        Pi = P[int(0.8 * N):]
+        Ce = Synapses(Pe, P, on_pre='ge+=we')
+        Ci = Synapses(Pi, P, on_pre='gi+=wi')
+        Ce.connect(p=80.0/N)
+        Ci.connect(p=80.0/N)
+
+        s_mon = SpikeMonitor(P)
+
+        self.timed_run(self.duration)
+
+
+class STDP(SpeedTest):
+
+    category = "Full examples"
+    name = "STDP with Poisson input"
+    tags = ["Neurons", "Synapses", "SpikeMonitor", "PoissonGroup"]
+    n_range = [100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000]
+    n_label = 'Num neurons'
+
+    # configuration options
+    duration = 1 * second
+
+    def run(self):
+        N = self.n
+        taum = 10 * ms
+        taupre = 20 * ms
+        taupost = taupre
+        Ee = 0 * mV
+        vt = -54 * mV
+        vr = -60 * mV
+        El = -74 * mV
+        taue = 5 * ms
+        F = 15 * Hz
+        gmax = .01
+        dApre = .01
+        dApost = -dApre * taupre / taupost * 1.05
+        dApost *= gmax
+        dApre *= gmax
+
+        eqs_neurons = '''
+        dv/dt = (ge * (Ee-vr) + El - v) / taum : volt
+        dge/dt = -ge / taue : 1
+        '''
+
+        input = PoissonGroup(N, rates=F)
+        neurons = NeuronGroup(1, eqs_neurons, threshold='v>vt', reset='v = vr',
+                              method='linear')
+        S = Synapses(input, neurons,
+                     '''w : 1
+                        dApre/dt = -Apre / taupre : 1 (event-driven)
+                        dApost/dt = -Apost / taupost : 1 (event-driven)''',
+                     on_pre='''ge += w
+                            Apre += dApre
+                            w = clip(w + Apost, 0, gmax)''',
+                     on_post='''Apost += dApost
+                             w = clip(w + Apre, 0, gmax)''',
+                     )
+        S.connect()
+        S.w = 'rand() * gmax'
+        s_mon = SpikeMonitor(input)
 
         self.timed_run(self.duration)
 
