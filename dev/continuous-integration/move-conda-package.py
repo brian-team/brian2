1d756f01fa6c76ad78aedd8fea44ab2643bba6c0
Marcel Stimberg <marcel.stimberg@inserm.fr>
2016-09-01 11:22:23 +0200

    Try to make the `move-conda-package.py` script work with the latest version of conda-build

--- dev/continuous-integration/move-conda-package.py
+++ dev/continuous-integration/move-conda-package.py
@@ -3,16 +3,15 @@ import os
 import yaml
 import glob
 import shutil
-try:
-    from conda_build.config import config
-except ImportError:
-    # For older versions of conda-build
-    from conda_build import config
+
+from conda_build.config import get_or_merge_config
 
 with open(os.path.join(sys.argv[1], 'meta.yaml')) as f:
     name = yaml.load(f)['package']['name']
 
-binary_package_glob = os.path.join(config.bldpkgs_dir, '{0}*.tar.bz2'.format(name))
+packages_dir = get_or_merge_config(None).bldpkgs_dir
+
+binary_package_glob = os.path.join(packages_dir, '{0}*.tar.bz2'.format(name))
 binary_packages = glob.glob(binary_package_glob)
 for binary_package in binary_packages:
     shutil.move(binary_package, '.')
