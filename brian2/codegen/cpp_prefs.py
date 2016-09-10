975d5188488dcf9b0ef6956d0b95f25e129735e3
thesamovar <dg.github@thesamovar.net>
2016-09-10 01:03:52 +0100

    Remove /fp:fast as well, seems to lead to same bug in cpp_standalone

--- brian2/codegen/cpp_prefs.py
+++ brian2/codegen/cpp_prefs.py
@@ -56,7 +56,7 @@ prefs.register_preferences(
         '''
         ),
     extra_compile_args_msvc=BrianPreference(
-        default=['/Ox', '/w', '/fp:fast', msvc_arch_flag],
+        default=['/Ox', '/w', msvc_arch_flag],
         docs='''
         Extra compile arguments to pass to MSVC compiler (the default
         ``/arch:`` flag is determined based on the processor architecture)
