5a2b6e68db1d24345330d36987653ec3122ce482
Marcel Stimberg <marcel.stimberg@inserm.fr>
2017-03-15 10:11:08 +0100

    Make sure to not mix integers and floats in the SI prefix scales -- newer numpy versions no longer allow 1 ** -1

--- brian2/units/fundamentalunits.py
+++ brian2/units/fundamentalunits.py
@@ -296,7 +296,7 @@ _iclass_label = ["metre", "kilogram", "second", "amp", "kelvin", "mole",
 
 # SI unit _prefixes, see table at end of file
 _siprefixes = {"y": 1e-24, "z": 1e-21, "a": 1e-18, "f": 1e-15, "p": 1e-12,
-               "n": 1e-9, "u": 1e-6, "m": 1e-3, "c": 1e-2, "d": 1e-1, "": 1,
+               "n": 1e-9, "u": 1e-6, "m": 1e-3, "c": 1e-2, "d": 1e-1, "": 1.0,
                "da": 1e1, "h": 1e2, "k": 1e3, "M": 1e6, "G": 1e9, "T": 1e12,
                "P": 1e15, "E": 1e18, "Z": 1e21, "Y": 1e24}
 
