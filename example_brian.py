40f0064678644dd1eb36d5e29a20084e1690f426
CharleeSF <charleetje@gmail.com>
2017-06-01 14:41:46 +0200

    Trying to link gsl library to cython compilation but is not really working so far

---
+++ example_brian.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+'''
+Input-Frequency curve of a IF model.
+Network: 1000 unconnected integrate-and-fire neurons (leaky IF)
+with an input parameter v0.
+The input is set differently for each neuron.
+'''
+from brian2 import *
+
+prefs.codegen.target = 'cython'
+
+prefs.codegen.cpp.libraries += ['gsl', 'gslcblas']
+prefs.codegen.cpp.extra_compile_args_gcc = ['-lgsl', '-lgslcblas']
+prefs.codegen.cpp.extra_link_args = ['-lgsl', '-lgslcblas']
+prefs.codegen.cpp.library_dirs += ['/home/charlee/softwarefolder/gsl-2.3/gsl/']
+prefs['codegen.runtime.cython.multiprocess_safe'] = False
+
+n = 1000
+duration = 1*second
+tau = 10*ms
+eqs = '''
+dv/dt = (v0 - v) / tau : volt (unless refractory)
+v0 : volt
+'''
+group = NeuronGroup(n, eqs, threshold='v > 10*mV', reset='v = 0*mV',
+                    refractory=5*ms, method='euler')
+group.v = 0*mV
+group.v0 = '20*mV * i / (n-1)'
+
+monitor = SpikeMonitor(group)
+
+run(duration)
+plot(group.v0/mV, monitor.count / duration)
+xlabel('v0 (mV)')
+ylabel('Firing rate (sp/s)')
+show()
