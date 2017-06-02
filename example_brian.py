2b7a68704299dab41900e8871ff40736392a0e9d
CharleeSF <charleetje@gmail.com>
2017-06-02 18:36:26 +0200

    StateUpdater class like this is not going to work, removed all and trying different path (see commit to cythoncode.pyx)

--- example_brian.py
+++ example_brian.py
@@ -16,50 +16,14 @@ prefs.codegen.cpp.include_dirs += ['/home/charlee/softwarefolder/gsl-2.3/gsl/']
 n = 10
 duration = .1*second
 tau = 10*ms
+
 eqs = '''
 dv/dt = (v0 - v) / tau : volt (unless refractory)
 v0 : volt
 '''
 
-#cdef int dimension = 2
-#cdef double y[2]
-#cdef gsl_odeiv2_system sysmbo
-#cdef gsl_odeiv2_driver * d
-
-external_library_functions = {'gsl_odeiv_alloc_y_new' : 'gsl_odeiv_alloc_y_new',
-                              'gsl_odeiv2_step_rk2' : 'gsl_odeiv2_step_rk2'}
-
-class GSLStateUpdater(StateUpdateMethod):
-    def __call__(self, equations, variables=None):
-        return '''
-sys.function = func
-sys.dimension = 2
-sys.params = &tau
-d = gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_rk2,1e-6, 1e-6, 0.0)
-not_refractory = (t - lastspike) > .005
-y[0] = v
-y[1] = v0
-t1 = t + dt
-status = gsl_odeiv2_driver_apply(d, &t, t1, y)
-_v = y[0]
-'''
-
-from brian2.core.functions import SymbolicConstant
-DEFAULT_CONSTANTS['gsl_odeiv2_step_rk2'] = SymbolicConstant('gsl_odeiv2_step_rk2','gsl_odeiv2_step_rk2', value='gsl_odeiv2_step_rk2')
-DEFAULT_CONSTANTS['gsl_odeiv2_driver_apply'] = SymbolicConstant('gsl_odeiv2_driver_apply','gsl_odeiv2_driver_apply', value='gsl_odeiv2_driver_apply')
-DEFAULT_CONSTANTS['gsl_odeiv2_driver_alloc_y_new'] = SymbolicConstant('gsl_odeiv2_driver_alloc_y_new','gsl_odeiv2_driver_alloc_y_new', value='gsl_odeiv2_driver_alloc_y_new')
-DEFAULT_CONSTANTS['sys'] = SymbolicConstant('sys','sys', value='sys')
-DEFAULT_CONSTANTS['func'] = SymbolicConstant('func','func', value='func')
-DEFAULT_CONSTANTS['y'] = SymbolicConstant('y','y', value='y')
-prefs.codegen.loop_invariant_optimisations = False
-
 group = NeuronGroup(n, eqs, threshold='v > 10*mV', reset='v = 0*mV',
-                    refractory=5*ms, method=GSLStateUpdater(), namespace=external_library_functions)
-
-#group = NeuronGroup(n, eqs, threshold='v > 10*mV', reset='v = 0*mV',
-#                    refractory=5*ms, method='euler')
-
-
+                    refractory=5*ms, method='euler')
 
 group.v = 0*mV
 group.v0 = '20*mV * i / (n-1)'
