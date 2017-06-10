f83d0bdfd47df7cd6622523db8081640b9cebd6c
CharleeSF <charleetje@gmail.com>
2017-06-10 15:05:26 +0200

    Gives the error: An error occurred when preparing an object. SyntaxError: All writes to scalar variables in a code block have to be made before writes to vector variables. Illegal write to tau. which I will try to solve by splitting scalar and vector code

--- example_brian.py
+++ example_brian.py
@@ -6,101 +6,12 @@ with an input parameter v0.
 The input is set differently for each neuron.
 '''
 from brian2 import *
+from brian2.core.variables import Variables, ArrayVariable, DynamicArrayVariable
+from brian2.utils.stringtools import get_identifiers
+import re
+from brian2 import DEFAULT_FUNCTIONS, DEFAULT_CONSTANTS
 
 tau = 10*ms
-
-@implementation('cython', code='''
-from libc.stdlib cimport malloc, free
-
-cdef extern from "gsl/gsl_odeiv2.h":
-    # gsl_odeiv2_system
-    ctypedef struct gsl_odeiv2_system:
-        int (* function) (double t,  double y[], double dydt[], void * params) nogil
-        int (* jacobian) (double t,  double y[], double * dfdy, double dfdt[], void * params) nogil
-        size_t dimension
-        void * params
-
-    ctypedef struct gsl_odeiv2_step
-    ctypedef struct gsl_odeiv2_control
-    ctypedef struct gsl_odeiv2_evolve
-    ctypedef struct gsl_odeiv2_driver
-
-    ctypedef struct gsl_odeiv2_step_type
-
-    gsl_odeiv2_step_type *gsl_odeiv2_step_rk2
-
-    int gsl_odeiv2_driver_apply(
-        gsl_odeiv2_driver *d, double *t, double t1, double y[]) nogil
-
-    gsl_odeiv2_driver *gsl_odeiv2_driver_alloc_y_new(
-        gsl_odeiv2_system *sys, gsl_odeiv2_step_type *T,
-        double hstart, double epsabs, double epsrel) nogil
-
-    int gsl_odeiv2_driver_free(gsl_odeiv2_driver *d) nogil
-
-cdef struct parameters:
-    double tau
-
-cdef struct statevar_container:
-    double* v
-    double* v0
-
-cdef double* assign_memory_y():
-	return <double *>malloc(2 * sizeof(double))
-
-cdef int assign_statevariable_arrays(_namespace, statevar_container* statevariables):
-    cdef _numpy.ndarray[double, ndim=1, mode='c'] _buf__array_neurongroup_v = _namespace['_array_neurongroup_v']
-    cdef double * _array_neurongroup_v = <double *> _buf__array_neurongroup_v.data
-    cdef _numpy.ndarray[double, ndim=1, mode='c'] _buf__array_neurongroup_v0 = _namespace['_array_neurongroup_v0']
-    cdef double * _array_neurongroup_v0 = <double *> _buf__array_neurongroup_v0.data
-
-    statevariables.v = _array_neurongroup_v
-    statevariables.v0 = _array_neurongroup_v0
-
-    return 0
-
-cdef int empty_statevariable_arrays(_namespace, statevar_container* statevariables):
-    cdef _numpy.ndarray[double, ndim=1, mode='c'] _buf__array_neurongroup_v = _namespace['_array_neurongroup_v']
-    cdef double * _array_neurongroup_v = <double *> _buf__array_neurongroup_v.data
-    cdef _numpy.ndarray[double, ndim=1, mode='c'] _buf__array_neurongroup_v0 = _namespace['_array_neurongroup_v0']
-    cdef double * _array_neurongroup_v0 = <double *> _buf__array_neurongroup_v0.data
-
-    _array_neurongroup_v = statevariables.v
-    _array_neurongroup_v0 = statevariables.v0
-
-    return 0
-
-cdef fill_odeiv2_system(_namespace, gsl_odeiv2_system *sys):
-    cdef double tau = _namespace["tau"]
-    sys.dimension = 2
-    sys.params = &tau
-
-cdef int fill_y_vector(statevar_container* statevariables, double * y, int _idx):
-    cdef double v
-    v = statevariables.v[_idx]
-    cdef double v0
-    v0 =  statevariables.v0[_idx]
-    y[0] = v
-    y[1] = v0
-    return 0
-
-cdef int empty_y_vector(statevar_container* statevariables, double * y, int _idx) nogil:
-    statevariables.v[_idx] = y[0]
-    statevariables.v0[_idx] = y[1]
-    return 0
-
-cdef int func (double t, const double y[], double f[], void *params) nogil:
-    cdef double v, v0, tau
-    tau = (<double *> params)[0]
-    v = y[0]
-    v0 = y[1]
-    f[0] = (v0 - v)/10e-3
-    f[1] = 0
-''')
-@check_units(result=1)
-def GSL_functions():
-    raise NotImplementedError
-    return 0
 
 prefs.codegen.target = 'cython'
 
@@ -112,13 +23,58 @@ n = 10
 duration = .1*second
 
 eqs = '''
-dv/dt = (v0 - v) / tau : volt (unless refractory)
+dv/dt = (v1 - v) / tau : volt (unless refractory)
+v1 = v0*multi : volt
+multi = 2 : 1
 v0 : volt
 '''
 
 class GSLStateUpdater(StateUpdateMethod):
     def __call__(self, equations, variables=None):
-        return '''not_refractory = (t - lastspike) > .005'''
+        # the approach is to write all variables so they can
+        # be translated to GSL code (i.e. with indexing and pointers)
+        diff_eqs = equations.get_substituted_expressions(variables)
+
+        code = []
+        count_statevariables = 0
+        counter = {}
+        defined = [x[0] for x in equations.eq_expressions]
+
+        # differential equation variables (have to be changed to f[0] in
+        # lhs and y[0] in rhs
+        for name in equations.diff_eq_names:
+            new_name = device.get_array_name(variables[name])
+            code += ['{var_single} = _gsl_{var}_y{count}'.format(var_single=name, var=new_name, count=count_statevariables)]
+            counter[name] = count_statevariables
+            count_statevariables += 1
+
+        # all the non-differential statevariables added to parameter struct
+        # if array than take array name (and later save to params as pointer)
+        print equations, equations.names
+        for name in equations.names:
+            if name in defined:
+                continue
+            var = variables[name]
+            if isinstance(var, ArrayVariable):
+                new_name = device.get_array_name(var)
+                code += ['{var_single} = _gsl_{var}_p'.format(var_single=name, var=new_name)]
+            else:
+                code += ['{var} = _gsl_{var}_p'.format(var=name)]
+
+        for name in equations.identifiers:
+            if name in DEFAULT_FUNCTIONS or name in DEFAULT_CONSTANTS:
+                pass
+            else:
+                code += ['{var} = _gsl_{var}_p'.format(var=name)]
+
+        for name, expr in diff_eqs:
+            new_name = device.get_array_name(variables[name])
+
+            code += ['_gsl_{var}_f{count} = {expr}'.format(var=new_name,
+                                                           expr=expr,
+                                                           count=counter[name])]
+        print code
+        return ('\n').join(code)
 
 group = NeuronGroup(n, eqs, threshold='v > 10*mV', reset='v = 0*mV',
                     refractory=5*ms, method=GSLStateUpdater())
@@ -132,6 +88,7 @@ monitor = SpikeMonitor(group)
 mon2 = StateMonitor(group, 'v', record=True)
 
 run(duration)
+print group.state_updater.codeobj.code
 plot(group.v0/mV, monitor.count / duration)
 xlabel('v0 (mV)')
 ylabel('Firing rate (sp/s)')
