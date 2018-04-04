brian-team/brian2

commit 9c99e87e27958f3d8e4e606dd46a987aa3a871ac

Overwrite previously used memory with 0 when shrinking a DynamicArray
Otherwise, resizing it will access previously written memory
Fixes #906

brian2/memory/dynamicarray.py
@@ -5,8 +5,13 @@ from numpy import *
 
 __all__ = ['DynamicArray', 'DynamicArray1D']
 
-def getslices(shape):
-    return tuple(slice(0, x) for x in shape)
+
+def getslices(shape, from_start=True):
+    if from_start:
+        return tuple(slice(0, x) for x in shape)
+    else:
+        return tuple(slice(x, None) for x in shape)
+
 
 class DynamicArray(object):
     '''
@@ -116,7 +121,10 @@ class DynamicArray(object):
                 slices = getslices(self._data.shape)
                 newdata[slices] = self._data
                 self._data = newdata
-        self.data = self._data[getslices(newshape)]
+        # If we reduced the size, set the no longer used memory to 0
+        self._data[getslices(newshape, from_start=False)] = 0
+        # Reduce our view to the requested size if necessary
+        self.data = self._data[getslices(newshape, from_start=True)]
         self.shape = self.data.shape
 
     def resize_along_first(self, newshape):
@@ -134,7 +142,10 @@ class DynamicArray(object):
                 slices = getslices(self._data.shape)
                 newdata[slices] = self._data
                 self._data = newdata
-        self. data = self._data[slice(0, new_dimension)]
+        # If we reduced the size, set the no longer used memory to 0
+        self._data[new_dimension:] = 0
+        # Reduce our view to the requested size if necessary
+        self. data = self._data[:new_dimension]
         self.shape = newshape
 
     def shrink(self, newshape):
@@ -196,10 +207,13 @@ class DynamicArray1D(DynamicArray):
                 newdata = zeros(newdatashape, dtype=self.dtype)
                 newdata[:shape] = self.data
                 self._data = newdata
+        # If we reduced the size, set the no longer used memory to 0
+        self._data[newshape:] = 0
+        # Reduce our view to the requested size if necessary
         self.data = self._data[:newshape]
         self.shape = (newshape,)
-    
-            
+
+
 if __name__=='__main__':
     if 1:
         x = DynamicArray1D(2, use_numpy_resize=True)

brian2/tests/test_memory.py
@@ -19,7 +19,7 @@ def test_dynamic_array_1d_access():
 
 
 @attr('codegen-independent')
-def test_dynamic_array_1d_resize():
+def test_dynamic_array_1d_resize_up_down():
     for numpy_resize in [True, False]:
         da = DynamicArray1D(10, use_numpy_resize=numpy_resize)
         da[:] = np.arange(10)
@@ -33,6 +33,20 @@ def test_dynamic_array_1d_resize():
         assert da.shape == (5, )
         assert all(da[:] == np.arange(5))
 
+@attr('codegen-independent')
+def test_dynamic_array_1d_resize_down_up():
+    for numpy_resize in [True, False]:
+        da = DynamicArray1D(10, use_numpy_resize=numpy_resize)
+        da[:] = np.arange(10)
+        da.resize(5)
+        assert len(da) == 5
+        assert da.shape == (5, )
+        assert all(da[:5] == np.arange(5))
+        da.resize(10)
+        assert len(da) == 10
+        assert da.shape == (10, )
+        assert all(da[:5] == np.arange(5))
+        assert all(da[5:] == 0)
 
 @attr('codegen-independent')
 def test_dynamic_array_1d_shrink():
@@ -60,7 +74,7 @@ def test_dynamic_array_2d_access():
 
 
 @attr('codegen-independent')
-def test_dynamic_array_2d_resize():
+def test_dynamic_array_2d_resize_up_down():
     for numpy_resize in [True, False]:
         da = DynamicArray((10, 20), use_numpy_resize=numpy_resize)
         da[:, :] = np.arange(200).reshape((10, 20))
@@ -79,6 +93,26 @@ def test_dynamic_array_2d_resize():
 
 
 @attr('codegen-independent')
+def test_dynamic_array_2d_resize_down_up():
+    for numpy_resize in [True, False]:
+        da = DynamicArray((10, 20), use_numpy_resize=numpy_resize)
+        da[:, :] = np.arange(200).reshape((10, 20))
+        da.resize((5, 20))
+        assert da.shape == (5, 20)
+        assert_equal(da, np.arange(100).reshape((5, 20)))
+        da.resize((5, 15))
+        assert da.shape == (5, 15)
+        for row_idx, row in enumerate(da):
+            assert_equal(row, 20*row_idx + np.arange(15))
+
+        da.resize((10, 20))
+        assert da.shape == (10, 20)
+        for row_idx, row in enumerate(da[:5, :15]):
+            assert_equal(row, 20*row_idx + np.arange(15))
+        assert_equal(da[5:, 15:], 0)
+
+
+@attr('codegen-independent')
 def test_dynamic_array_2d_shrink():
     for numpy_resize in [True, False]:
         da = DynamicArray((10, 20), use_numpy_resize=numpy_resize)
@@ -92,8 +126,10 @@ def test_dynamic_array_2d_shrink():
 
 if __name__=='__main__':
     test_dynamic_array_1d_access()
-    test_dynamic_array_1d_resize()
+    test_dynamic_array_1d_resize_up_down()
+    test_dynamic_array_1d_resize_down_up()
     test_dynamic_array_1d_shrink()
     test_dynamic_array_2d_access()
-    test_dynamic_array_2d_resize()
+    test_dynamic_array_2d_resize_up_down()
+    test_dynamic_array_2d_resize_down_up()
     test_dynamic_array_2d_shrink()

commit 8595f23a9dc929babdf2b68ba0d5ead898fccb97

Do not unnecessarily set values in dynamic arrays to 0
Fixes a performance regression introduced by commit 9c99e87e279

brian2/memory/dynamicarray.py
@@ -121,8 +121,9 @@ class DynamicArray(object):
                 slices = getslices(self._data.shape)
                 newdata[slices] = self._data
                 self._data = newdata
-        # If we reduced the size, set the no longer used memory to 0
-        self._data[getslices(newshape, from_start=False)] = 0
+        elif (newshapearr < self.shape).any():
+            # If we reduced the size, set the no longer used memory to 0
+            self._data[getslices(newshape, from_start=False)] = 0
         # Reduce our view to the requested size if necessary
         self.data = self._data[getslices(newshape, from_start=True)]
         self.shape = self.data.shape
@@ -142,8 +143,9 @@ class DynamicArray(object):
                 slices = getslices(self._data.shape)
                 newdata[slices] = self._data
                 self._data = newdata
-        # If we reduced the size, set the no longer used memory to 0
-        self._data[new_dimension:] = 0
+        elif newshape < self.shape:
+            # If we reduced the size, set the no longer used memory to 0
+            self._data[new_dimension:] = 0
         # Reduce our view to the requested size if necessary
         self. data = self._data[:new_dimension]
         self.shape = newshape