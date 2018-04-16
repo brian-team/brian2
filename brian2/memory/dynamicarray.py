1fd324d22fc6b46beaf22a5368bba16965bb3aa1
Marcel Stimberg <marcel.stimberg@inserm.fr>
2018-04-16 18:55:06 +0200

    Also apply the fix from #936 to 1D arrays (less performance impact, but still...)

--- brian2/memory/dynamicarray.py
+++ brian2/memory/dynamicarray.py
@@ -209,8 +209,9 @@ class DynamicArray1D(DynamicArray):
                 newdata = zeros(newdatashape, dtype=self.dtype)
                 newdata[:shape] = self.data
                 self._data = newdata
-        # If we reduced the size, set the no longer used memory to 0
-        self._data[newshape:] = 0
+        elif newshape < self.shape[0]:
+            # If we reduced the size, set the no longer used memory to 0
+            self._data[newshape:] = 0
         # Reduce our view to the requested size if necessary
         self.data = self._data[:newshape]
         self.shape = (newshape,)
