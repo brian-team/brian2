c89c6bc71de42fca8cb1d98bc3301ccbc85a76bc
Marcel Stimberg <marcel.stimberg@inserm.fr>
2018-05-30 14:36:30 +0200

    Improve `repo_stats` script
    [ci skip]

--- dev/tools/repo_stats.py
+++ dev/tools/repo_stats.py
@@ -10,18 +10,20 @@ if API_TOKEN is None:
     raise ValueError('Need to specify an API token')
 
 p = Github(API_TOKEN)
-last_release = datetime.datetime(year=2017, month=6, day=8)
+last_release = datetime.datetime(year=2017, month=11, day=8)
 authors = []
 comments = p.get_repo('brian-team/brian2').get_issues_comments(since=last_release)
 comment_counter = 0
 for comment in comments:
     name = comment.user.name
     if name is None:
-        name = '???'
+        authors.append('`@{login} <https://github.com/{login}>`_'.format(login=comment.user.login.encode('utf-8'),
+                                                                         name=name))
     else:
-        name = name.encode('utf-8')
-    authors.append('{name} (`@{login} <https://github.com/{login}>`_)'.format(login=comment.user.login.encode('utf-8'),
-                                                                                name=name))
+        authors.append(
+            '{name} (`@{login} <https://github.com/{login}>`_)'.format(
+                login=comment.user.login.encode('utf-8'),
+                name=name.encode('utf-8')))
     comment_counter += 1
 print('Counted {} comments'.format(comment_counter))
 
@@ -30,11 +32,13 @@ issue_counter = 0
 for issue in issues:
     name = issue.user.name
     if name is None:
-        name = '???'
+        authors.append('`@{login} <https://github.com/{login}>`_'.format(login=issue.user.login.encode('utf-8'),
+                                                                         name=name))
     else:
-        name = name.encode('utf-8')
-    authors.append('{name} (`@{login} <https://github.com/{login}>`_)'.format(login=issue.user.login.encode('utf-8'),
-                                                                                name=name))
+        authors.append(
+            '{name} (`@{login} <https://github.com/{login}>`_)'.format(
+                login=issue.user.login.encode('utf-8'),
+                name=name.encode('utf-8')))
     issue_counter += 1
 print('Counted {} issues'.format(issue_counter))
 
