f9d23f42e6ab127dadd4f14615d2b1c7469adfa1
Marcel Stimberg <marcel.stimberg@inserm.fr>
2018-06-13 12:40:03 +0200

    Reduce number of names imported via a wildcard import from the `monitor` package

--- brian2/monitors/__init__.py
+++ brian2/monitors/__init__.py
@@ -1,3 +1,10 @@
+'''
+Base package for all monitors, i.e. objects to record activity during a
+simulation run.
+'''
 from spikemonitor import *
 from statemonitor import *
 from ratemonitor import *
+
+__all__ = ['SpikeMonitor', 'EventMonitor', 'StateMonitor',
+           'PopulationRateMonitor']
