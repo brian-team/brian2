2a6e90bdd63a3bb29d8b671581c00c5bf2a01957
Marcel Stimberg <marcel.stimberg@inserm.fr>
2018-07-12 11:54:39 +0900

    Test 32bit float dtype on the CI servers

--- dev/continuous-integration/run_test_suite.py
+++ dev/continuous-integration/run_test_suite.py
@@ -6,6 +6,8 @@ the logic into Windows batch/bash statements)
 import os
 import sys
 
+import numpy as np
+
 import brian2
 
 split_run = os.environ.get('SPLIT_RUN', None)
@@ -14,6 +16,7 @@ standalone = os.environ.get('STANDALONE', 'no').lower() in ['yes', 'true']
 operating_system = os.environ.get('TRAVIS_OS_NAME', 'windows').lower()
 cross_compiled = os.environ.get('CROSS_COMPILED', 'FALSE').lower() in ['yes', 'true']
 report_coverage = os.environ.get('REPORT_COVERAGE', 'no').lower() in ['yes', 'true']
+dtype_32_bit = os.environ.get('FLOAT_DTYPE_32', 'no').lower() in ['yes', 'true']
 
 if split_run == '1':
     targets = ['numpy', 'weave']
@@ -36,19 +39,26 @@ else:
     openmp = False
 
 reset_preferences = not cross_compiled
+if dtype_32_bit:
+    float_dtype = np.float32
+else:
+    float_dtype = None
+
 if standalone:
     result = brian2.test([],
                          test_codegen_independent=False,
                          test_standalone='cpp_standalone',
                          test_openmp=openmp,
                          test_in_parallel=in_parallel,
-                         reset_preferences=reset_preferences)
+                         reset_preferences=reset_preferences,
+                         float_dtype=float_dtype)
 else:
     result = brian2.test(targets,
                          test_codegen_independent=independent,
                          test_standalone=None,
                          test_in_parallel=in_parallel,
-                         reset_preferences=reset_preferences)
+                         reset_preferences=reset_preferences,
+                         float_dtype=float_dtype)
 
 if not result:
     sys.exit(1)
