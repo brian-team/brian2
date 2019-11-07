ad69cb153dcace9266da097ee6079b5b0cee90a6
Marcel Stimberg <marcel.stimberg@inserm.fr>
2019-11-07 15:52:19 +0100

    Use numpy for doctests in .rst files

--- docs_sphinx/conftest.py
+++ docs_sphinx/conftest.py
@@ -4,7 +4,10 @@ import pytest
 @pytest.fixture(autouse=True)
 def add_brian2(doctest_namespace):
     exec('from brian2 import *', doctest_namespace)
+    import brian2
     import numpy as np
+    # Always use numpy for doctests
+    brian2.prefs['codegen.target'] = 'numpy'
     # Print output changed in numpy 1.14, stick with the old format to
     # avoid doctest failures
     try:
