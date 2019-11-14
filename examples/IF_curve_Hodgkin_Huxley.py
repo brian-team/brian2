28baceb8d2ab49e7ca1b3e4948a5c3008d92476c
Marcel Stimberg <marcel.stimberg@inserm.fr>
2019-11-14 12:01:23 +0100

    Use exprel in examples

--- examples/IF_curve_Hodgkin_Huxley.py
+++ examples/IF_curve_Hodgkin_Huxley.py
@@ -25,11 +25,8 @@ VT = -63*mV
 # The model
 eqs = Equations('''
 dv/dt = (gl*(El-v) - g_na*(m*m*m)*h*(v-ENa) - g_kd*(n*n*n*n)*(v-EK) + I)/Cm : volt
-dm/dt = 0.32*(mV**-1)*(13.*mV-v+VT)/
-    (exp((13.*mV-v+VT)/(4.*mV))-1.)/ms*(1-m)-0.28*(mV**-1)*(v-VT-40.*mV)/
-    (exp((v-VT-40.*mV)/(5.*mV))-1.)/ms*m : 1
-dn/dt = 0.032*(mV**-1)*(15.*mV-v+VT)/
-    (exp((15.*mV-v+VT)/(5.*mV))-1.)/ms*(1.-n)-.5*exp((10.*mV-v+VT)/(40.*mV))/ms*n : 1
+dm/dt = 0.32*(mV**-1)*4*mV/exprel((13.*mV-v+VT)/(4*mV))/ms*(1-m)-0.28*(mV**-1)*5*mV/exprel((v-VT-40.*mV)/(5*mV))/ms*m : 1
+dn/dt = 0.032*(mV**-1)*5*mV/exprel((15.*mV-v+VT)/(5*mV))/ms*(1.-n)-.5*exp((10.*mV-v+VT)/(40.*mV))/ms*n : 1
 dh/dt = 0.128*exp((17.*mV-v+VT)/(18.*mV))/ms*(1.-h)-4./(1+exp((40.*mV-v+VT)/(5.*mV)))/ms*h : 1
 I : amp
 ''')
