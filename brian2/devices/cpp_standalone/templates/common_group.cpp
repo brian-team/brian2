a23c12999f08004b04fc23134f6cf998730c9cdb
Marcel Stimberg <marcel.stimberg@inserm.fr>
2020-02-06 17:44:07 +0100

    Be more consistent in standalone templates
    maincode block in common_groups had code that was only used by a single template.
    Now, each template overwrites the maincode block.

--- brian2/devices/cpp_standalone/templates/common_group.cpp
+++ brian2/devices/cpp_standalone/templates/common_group.cpp
@@ -39,22 +39,7 @@ void _run_{{codeobj_name}}()
     {{pointers_lines|autoindent}}
 
     {% block maincode %}
-    //// MAIN CODE ////////////
-    // scalar code
-    const size_t _vectorisation_idx = -1;
-    {{scalar_code|autoindent}}
-
-    {# N is a constant in most cases (NeuronGroup, etc.), but a scalar array for
-       synapses, we therefore have to take care to get its value in the right
-       way. #}
-    const int _N = {{constant_or_scalar('N', variables['N'])}};
-    {{openmp_pragma('parallel-static')}}
-    for(int _idx=0; _idx<_N; _idx++)
-    {
-        // vector code
-        const size_t _vectorisation_idx = _idx;
-        {{vector_code|autoindent}}
-    }
+    {# Will be overwritten in child templates #}
     {% endblock %}
 
     {% if profiled %}
