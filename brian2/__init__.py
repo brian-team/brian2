a9766792f9a0c3f678bf481fc0958c735cf99afc
Marcel Stimberg <marcel.stimberg@inserm.fr>
2020-09-28 17:40:56 +0200

    ***** Release Brian2 2.4.1 *****

--- brian2/__init__.py
+++ brian2/__init__.py
@@ -60,8 +60,8 @@ if 'rate' in globals():
 
 __docformat__ = "restructuredtext en"
 
-__version__ = '2.4+git'
-__release_date__ = '2020-09-04'
+__version__ = '2.4.1'
+__release_date__ = '2020-09-28'
 
 from brian2.only import *
 from brian2.only import test
