05386c2cb26526ccea55a85a0d0c39a90deb2295
Marcel Stimberg <marcel.stimberg@inserm.fr>
2020-10-27 17:39:21 +0100

    Use a PyPI-compatible development version

--- brian2/__init__.py
+++ brian2/__init__.py
@@ -60,7 +60,7 @@ if 'rate' in globals():
 
 __docformat__ = "restructuredtext en"
 
-__version__ = '2.4.1+git'
+__version__ = '2.4.1.dev0'
 __release_date__ = '2020-09-28'
 
 from brian2.only import *
