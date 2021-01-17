81c5d322596945681d083e6a9f05f47ed1654efc
Abolfazl Ziaeemehr <a.ziaeemehr@gmail.com>
2021-01-17 21:44:55 +0330

    standalone simple case added.

---
+++ examples/standalone/simple_case.py
@@ -0,0 +1,16 @@
+import numpy as np
+import brian2 as b2
+import pylab as plt
+
+b2.start_scope()
+b2.set_device('cpp_standalone', build_on_run=False)
+# b2.prefs.codegen.target = 'numpy'  # use the Python fallback
+
+tau = 10*b2.ms
+eqs = '''
+dv/dt = (1-v)/tau : 1
+'''
+G = b2.NeuronGroup(1, eqs, method='exact')
+b2.run(100*b2.ms)
+b2.device.build(directory='output', compile=True, run=True, debug=False)
+print('After v = %s' % G.v[0])
