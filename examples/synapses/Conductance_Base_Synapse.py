a459a746febd42a6fb729637badde8a27e32586b
Abolfazl Ziaeemehr <a.ziaeemehr@gmail.com>
2021-01-18 16:58:54 +0330

    conductance base synapse modified.

--- examples/synapses/Conductance_Base_Synapse.py
+++ examples/synapses/Conductance_Base_Synapse.py
@@ -1,11 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
+'''
+Conductance Base Synapse with Brain2
+------------------------------------
 
+This example implement conductance base synapse. The synapse gating variable updates at each time step.
+Two RTM neuron are connected to each other through a directed synapse from neuron 1 to 2.
+
+Here, the complexity rather stems from the fact that the synaptic conductance is a continuous function of the membrane potential, instead of being triggered by individual spikes. This can be useful in particular when analyzing models mathematically but it is not recommended in most cases because they tend to be less efficient.
+Also note that this model only works with (pre-synaptic) neuron models that model the action potential in detail, i.e. not with integrate-and-fire type models.
+
+There are two broad approaches (`s` as part of the pre-synaptic neuron or `s` as part of the Synapses object), all depends on whether your time constants are the same across all synapses or whether they can vary between synapses.
+
+Reference:
+
+-  Introduction to modeling neural dynamics, Borgers, chapter 20
+- [LINK to discussion](https://brian.discourse.group/t/how-to-implement-a-conductance-base-synapse/77/2)
+
+'''
 # ### Conductance Base Synapse with Brain2
-# 
-# #### Reference:
-# -  Introduction to modeling neural dynamics, Borgers, chapter 20
-# - [LINK to discussion](https://brian.discourse.group/t/how-to-implement-a-conductance-base-synapse/77/2)
+#
 
 from brian2 import *
 import matplotlib.pyplot as plt
@@ -22,12 +34,14 @@ def plot_data(state_monitor, title=None, c='k'):
 
     fig, ax = plt.subplots(2, figsize=(10, 6), sharex=True)
 
-    
-    ax[0].plot(state_monitor.t / ms, state_monitor.vm[0] / mV, lw=2, c="r", alpha=0.5, label="neuron 0")
-    ax[0].plot(state_monitor.t / ms, state_monitor.vm[1] / mV, lw=2, c="b", alpha=0.5, label='neuron 1')
-    ax[1].plot(state_monitor.t / ms, state_monitor.s_in[0], lw=2, c="r", label='s_in, source')
-    ax[1].plot(state_monitor.t / ms, state_monitor.s_in[1], lw=2, c="b", label='s_in, target')
-    
+    ax[0].plot(state_monitor.t / ms, state_monitor.vm[0] /
+               mV, lw=2, c="r", alpha=0.5, label="neuron 0")
+    ax[0].plot(state_monitor.t / ms, state_monitor.vm[1] /
+               mV, lw=2, c="b", alpha=0.5, label='neuron 1')
+    ax[1].plot(state_monitor.t / ms, state_monitor.s_in[0],
+               lw=2, c="r", label='s_in, source')
+    ax[1].plot(state_monitor.t / ms, state_monitor.s_in[1],
+               lw=2, c="b", label='s_in, target')
 
     ax[0].set_xlabel("t [ms]")
     ax[0].set_ylabel("v [mV]")
@@ -42,26 +56,26 @@ def plot_data(state_monitor, title=None, c='k'):
     if title is not None:
         ax[0].set_title(title)
 
-    plt.show();
+    plt.show()
 
 
 def RTM_CELL(N, I_e, simulation_time):
-    
+
     # neuron RTM parameters
     El = -67 * mV
     EK = -100 * mV
     ENa = 50 * mV
-    ESyn= 0 * mV
+    ESyn = 0 * mV
     gl = 0.1 * msiemens
     gK = 80 * msiemens
     gNa = 100 * msiemens
-    
+
     C = 1 * ufarad
-    
-    weight = 0.25* msiemens
+
+    weight = 0.25 * msiemens
     gSyn = 1.0 * msiemens
-    tau_d = 2 *ms
-    tau_r = 0.2 *ms
+    tau_d = 2 * ms
+    tau_r = 0.2 * ms
 
     # forming RTM model with differential equations
     eqs = """
@@ -88,38 +102,36 @@ def RTM_CELL(N, I_e, simulation_time):
     dvm/dt = membrane_Im/C : volt
     """
 
-    neuron = NeuronGroup(N, eqs, method="euler", 
-                            dt=0.01*ms,
-                            threshold='vm>-55*mV')
-    
+    neuron = NeuronGroup(N, eqs, method="euler",
+                         dt=0.01*ms,
+                         threshold='vm>-55*mV')
+
     # initialize variables
     neuron.vm = [-70.0, -65.0]*mV
     neuron.m = "alpham / (alpham + betam)"
     neuron.h = "alphah / (alphah + betah)"
     neuron.n = "alphan / (alphan + betan)"
     neuron.I_ext = [I_e, 0.0*uA]
-    neuron.s_in=[0, 0]
-    
-    S = Synapses(neuron, 
-                    neuron, 
-                    '''
+    neuron.s_in = [0, 0]
+
+    S = Synapses(neuron,
+                 neuron,
+                 '''
                     w : 1
                     s_in_post = w*s_pre:1 (summed)
                     ''')
     S.connect(i=0, j=1)
     S.w[0, 1] = 0.25
-    
 
     # tracking variables
-    st_mon = StateMonitor(neuron, ["vm", "s", "s_in"], record=[0,1])
+    st_mon = StateMonitor(neuron, ["vm", "s", "s_in"], record=[0, 1])
 
     # running the simulation
     net = Network(neuron)
     net.add(st_mon)
     net.add(S)
     net.run(simulation_time)
-    return st_mon   
-
+    return st_mon
 
 
 st = RTM_CELL(2, 1.5*uA, 100*ms)
