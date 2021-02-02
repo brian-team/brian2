eafac2c327970d1fa03aa9255d63763b258c01ec
Marcel Stimberg <marcel.stimberg@inserm.fr>
2021-02-02 16:09:02 +0100

    Fix the style configuration

--- brian2/_version.py
+++ brian2/_version.py
@@ -40,7 +40,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "pep440-pre"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "brian2-"
     cfg.versionfile_source = "brian2/_version.py"
