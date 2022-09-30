c06852390d49f61ab3deeb1b7da5a3fac44f3cc3
Marcel Stimberg <marcel.stimberg@inserm.fr>
2022-09-30 06:46:54 -0700

    Use configured results directory for stdout etc.

--- brian2/devices/cpp_standalone/device.py
+++ brian2/devices/cpp_standalone/device.py
@@ -1129,7 +1129,7 @@ class CPPStandaloneDevice(Device):
                                 name_suffix='overwritten_env_var', once=True)
                 os.environ[key] = value
             if not with_output:
-                stdout = open('results/stdout.txt', 'w')
+                stdout = open(os.path.join(self.results_dir, 'stdout.txt'), 'w')
             else:
                 stdout = None
             if os.name == 'nt':
@@ -1148,13 +1148,15 @@ class CPPStandaloneDevice(Device):
             if stdout is not None:
                 stdout.close()
             if x:
-                if os.path.exists('results/stdout.txt'):
-                    with open('results/stdout.txt', 'r') as f:
+                stdout_fname = os.path.join(self.results_dir, 'stdout.txt')
+                if os.path.exists(stdout_fname):
+                    with open(stdout_fname, 'r') as f:
                         print(f.read())
                 raise RuntimeError(f"Project run failed with error code {x} (project directory: {os.path.abspath(directory)})")
             self.has_been_run = True
-            if os.path.isfile('results/last_run_info.txt'):
-                with open('results/last_run_info.txt', 'r') as f:
+            run_info_fname = os.path.join(self.results_dir, 'last_run_info.txt')
+            if os.path.isfile(run_info_fname):
+                with open(run_info_fname, 'r') as f:
                     last_run_info = f.read()
                 run_time, completed_fraction = last_run_info.split()
                 self._last_run_time = float(run_time)
