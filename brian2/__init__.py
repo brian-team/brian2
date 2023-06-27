7b4732357601db340c9f2f965706f97d68e280da
Marcel Stimberg <marcel.stimberg@inserm.fr>
2023-06-27 13:18:13 +0200

    Make setuptools_scm usage more robust

--- brian2/__init__.py
+++ brian2/__init__.py
@@ -1,6 +1,7 @@
 """
 Brian 2
 """
+import logging
 
 
 def _check_dependencies():
@@ -55,7 +56,26 @@ except ImportError:
 import brian2.numpy_ as numpy
 import brian2.numpy_ as np
 
-from ._version import __version__, __version_tuple__
+try:
+    from ._version import __version__, __version_tuple__
+except ImportError:
+    try:
+        from setuptools_scm import get_version
+
+        __version__ = get_version(
+            root="..",
+            relative_to=__file__,
+            version_scheme="post-release",
+            local_scheme="no-local-version",
+        )
+        __version_tuple__ = tuple(int(x) for x in __version__.split(".")[:3])
+    except ImportError:
+        logging.getLogger("brian2").warn(
+            "Cannot determine Brian version, running from source and "
+            "setuptools_scm is not installed."
+        )
+        __version__ = "unknown"
+        __version_tuple__ = (0, 0, 0)
 
 # delete some annoying names from the namespace
 if "x" in globals():
