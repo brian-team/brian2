brian-team/brian2

commit d3d9e1684b4308fe8dd3149251d00580d72792e0

Fix changes for better pickling of Synapses

brian2/synapses/synapses.py
@@ -980,10 +980,11 @@ class Synapses(Group):
         self._pathways = []
 
         if isinstance(on_event, str):
-            events_dict = {"pre": on_event, "post": on_event}
+            self.default_event = on_event
+            events_dict = {}
         else:
-            events_dict = {"pre": "spike", "post": "spike"}
-            events_dict.update(on_event)
+            self.default_event = "spike"
+            events_dict = dict(on_event)
 
         #: "Events" for all the pathways
         self.events = events_dict
@@ -993,7 +994,10 @@ class Synapses(Group):
             if isinstance(argument, str):
                 pathway_delay = delay.get(prepost, None)
                 self._add_updater(
-                    argument, prepost, delay=pathway_delay, event=self.events[prepost]
+                    argument,
+                    prepost,
+                    delay=pathway_delay,
+                    event=self.events.get(prepost, self.default_event),
                 )
             elif isinstance(argument, Mapping):
                 for key, value in argument.items():
@@ -1010,7 +1014,7 @@ class Synapses(Group):
                         prepost,
                         objname=key,
                         delay=pathway_delay,
-                        event=self.events[key],
+                        event=self.events.get(prepost, self.default_event),
                     )
 
         # Check whether any delays were specified for pathways that don't exist

commit 74df8a8d612e725f8df4775cb0d16282253fb725

Fix `on_event` with pathway names
This worked previously, but was broken with commit d3d9e1684b4308fe
See https://brian.discourse.group/t/how-to-activate-synaptic-pathways-on-custom-events-only/1213 and https://brian.discourse.group/t/reproducibility-issue-between-brian2-5-and-2-7/1240

brian2/synapses/synapses.py
@@ -1014,7 +1014,7 @@ class Synapses(Group):
                         prepost,
                         objname=key,
                         delay=pathway_delay,
-                        event=self.events.get(prepost, self.default_event),
+                        event=self.events.get(key, self.default_event),
                     )
 
         # Check whether any delays were specified for pathways that don't exist

brian2/tests/test_synapses.py
@@ -1058,6 +1058,37 @@ def test_transmission_custom_event():
     assert_allclose(mon[1].v[mon.t >= 1 * ms], 1.0)
 
 
+@pytest.mark.standalone_compatible
+def test_transmission_custom_event_complex():
+    # This was broken with release 2.6, entries in on_event where checked against pre/post
+    # instead of the pathway name
+    group = NeuronGroup(3, "v:1", threshold="v>1 and v<2", events={"over_2": "v>2"})
+    group.v = [0, 1.5, 2.5]
+
+    s = Synapses(
+        group,
+        group,
+        model="""a:integer
+                b:integer
+                c:integer
+                d:integer""",
+        on_event={
+            "path_a": "spike",
+            "path_b": "over_2",
+            "path_c": "spike",
+            "path_d": "over_2",
+        },
+        on_pre={"path_a": "a+=1", "path_b": "b+=1"},
+        on_post={"path_c": "c+=1", "path_d": "d+=1"},
+    )
+    s.connect()
+    run(defaultclock.dt)
+    assert all(s.a[:] == [0, 0, 0, 1, 1, 1, 0, 0, 0])
+    assert all(s.b[:] == [0, 0, 0, 0, 0, 0, 1, 1, 1])
+    assert all(s.c[:] == [0, 1, 0, 0, 1, 0, 0, 1, 0])
+    assert all(s.d[:] == [0, 0, 1, 0, 0, 1, 0, 0, 1])
+
+
 @pytest.mark.codegen_independent
 def test_invalid_custom_event():
     group1 = NeuronGroup(