441d7146d6825a77b283be785fe7c1d6449e1184
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2024-12-18 17:36:00 +0100

    Simplify release script by passing version as argument
    [ci skip]

--- .codemeta/create_codemeta.py
+++ .codemeta/create_codemeta.py
@@ -2,47 +2,51 @@ import re
 import os
 import pkg_resources
 import tomllib
-import setuptools_scm
 import json
+import sys
 
-basedir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 
-with open(os.path.join(basedir, "pyproject.toml"), "rb") as f:
-    pyproject = tomllib.load(f)
-with open(os.path.join(basedir, ".codemeta", "codemeta_base.json"), "r", encoding="utf-8") as f:
-    codemeta = json.load(f)
-with open(os.path.join(basedir, "AUTHORS"), "r", encoding="utf-8") as f:
-    authors = f.read().splitlines()[6:]
+if __name__ == "__main__":
+    if not len(sys.argv) == 2:
+        raise ValueError("Usage: python create_codemeta.py <version>")
+    basedir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 
-# Add software requirements from pyproject.toml
-parsed_deps = pkg_resources.parse_requirements(pyproject['project']['dependencies'])
-codemeta["softwareRequirements"] = []
-for dep in parsed_deps:
-    version = ",".join(f"{op}{v}" for op,v in dep.specs)
-    requirement = {"name": dep.project_name,"@type": "SoftwareApplication", "runtimePlatform": "Python 3"}
-    if version:
-        requirement["version"] = version
-    codemeta["softwareRequirements"].append(requirement)
+    with open(os.path.join(basedir, "pyproject.toml"), "rb") as f:
+        pyproject = tomllib.load(f)
+    with open(os.path.join(basedir, ".codemeta", "codemeta_base.json"), "r", encoding="utf-8") as f:
+        codemeta = json.load(f)
+    with open(os.path.join(basedir, "AUTHORS"), "r", encoding="utf-8") as f:
+        authors = f.read().splitlines()[6:]
 
-# Add contributors from AUTHORS
-codemeta["contributor"] = []
-for author in authors:
-    matches = re.match(r"^(\w[\w-]*?) ([\w]+[.]? )??(\w+) \(@(.*)\)$", author)
-    if not matches:
-        raise ValueError("author not matched:", author)
-    given_name, middle_name, family_name, github = matches.groups()
+    # Add software requirements from pyproject.toml
+    parsed_deps = pkg_resources.parse_requirements(pyproject['project']['dependencies'])
+    codemeta["softwareRequirements"] = []
+    for dep in parsed_deps:
+        version = ",".join(f"{op}{v}" for op,v in dep.specs)
+        requirement = {"name": dep.project_name,"@type": "SoftwareApplication", "runtimePlatform": "Python 3"}
+        if version:
+            requirement["version"] = version
+        codemeta["softwareRequirements"].append(requirement)
 
-    contributor = {"@type": "Person", "givenName": given_name, "familyName": family_name, "identifier": f"https://github.com/{github}"}
-    # FIXME: additionalName does not seem to be recognized by codemeta (validation fails)
-    if middle_name:
-        contributor["givenName"] += " " + middle_name.strip()
-    codemeta["contributor"].append(contributor)
+    # Add contributors from AUTHORS
+    codemeta["contributor"] = []
+    for author in authors:
+        matches = re.match(r"^(\w[\w-]*?) ([\w]+[.]? )??(\w+) \(@(.*)\)$", author)
+        if not matches:
+            raise ValueError("author not matched:", author)
+        given_name, middle_name, family_name, github = matches.groups()
 
-# Add version from setuptools_scm
-version = setuptools_scm.get_version(root=basedir)
-codemeta["version"] = version
-codemeta["softwareVersion"] = version
+        contributor = {"@type": "Person", "givenName": given_name, "familyName": family_name, "identifier": f"https://github.com/{github}"}
+        # FIXME: additionalName does not seem to be recognized by codemeta (validation fails)
+        if middle_name:
+            contributor["givenName"] += " " + middle_name.strip()
+        codemeta["contributor"].append(contributor)
 
-# Write codemeta.json
-with open(os.path.join(basedir, "codemeta.json"), "w", encoding="utf-8") as f:
-    json.dump(codemeta, f, indent=2, ensure_ascii=False)
+    # Add version from setuptools_scm
+    version = sys.argv[1]
+    codemeta["version"] = version
+    codemeta["softwareVersion"] = version
+
+    # Write codemeta.json
+    with open(os.path.join(basedir, "codemeta.json"), "w", encoding="utf-8") as f:
+        json.dump(codemeta, f, indent=2, ensure_ascii=False)
--- dev/tools/release/release.py
+++ dev/tools/release/release.py
@@ -8,14 +8,13 @@ basedir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)
 print('Current version is: ' + brian2.__version__)
 version = input('Enter new Brian2 version number: ').strip()
 
+# Run script to update codemeta.json
+os.system(f'python {basedir}/.codemeta/create_codemeta.py {version}')
+
 # commit
-os.system('git commit -a -v --allow-empty -m "***** Release Brian2 %s *****"' % version)
+os.system(f'git add {basedir}/codemeta.json && git commit -m "***** Release Brian2 {version} *****"')
 # add tag
 os.system(f'git tag -a -m "Release Brian2 {version}" {version}')
-# Run script to update codemeta.json
-os.system(f'python {basedir}/.codemeta/create_codemeta.py')
-# Include codemeta.json update in commit and update tag
-os.system(f'git add {basedir}/codemeta.json && git commit --amend --no-edit && git tag -f -a -m "Release Brian2 {version}" {version}')
 
 # print commands necessary for pushing
 print('Review the last commit: ')
