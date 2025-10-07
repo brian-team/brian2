d985c15f4669ac8212f23f95665930fd0112fa0b
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2025-10-07 19:59:14 +0530

    refactor: statemoniter template to use direct cpp resize methods instead of python indirection

--- brian2/codegen/runtime/cython_rt/templates/statemonitor.pyx
+++ brian2/codegen/runtime/cython_rt/templates/statemonitor.pyx
@@ -6,7 +6,7 @@
     cdef size_t _new_len = {{N}} + 1
 
     # Resize the recorded times
-    _var_t.resize(_new_len)
+    {{_dynamic_t_ptr}}.resize(_new_len)
     cdef double* _t_data = {{_dynamic_t_ptr}}.get_data_ptr()
     _t_data[_new_len-1] = {{_clock_t}}
 
@@ -18,10 +18,11 @@
 
     {% for varname, var in _recorded_variables | dictsort %}
     {% set c_type = cpp_dtype(variables[varname].dtype) %}
-    {% set np_type = numpy_dtype(variables[varname].dtype) %}
+    {% set array_name = get_array_name(var, access_data=False) %}
+
     # Resize the recorded variable "{{varname}}" and get the (potentially
     # changed) reference to the underlying data
-    _var_{{varname}}.resize((_new_len, _num{{_indices}}))
+    {{array_name}}_ptr.resize(_new_len, _num{{_indices}})
     cdef {{c_type}}* _record_data_{{varname}} = <{{c_type}}*> {{get_array_name(var, access_data=False) + "_ptr"}}.get_data_ptr()
     for _i in range(_num{{_indices}}):
         # vector code
