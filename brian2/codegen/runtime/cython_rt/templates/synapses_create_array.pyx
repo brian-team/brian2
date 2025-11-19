f5cbb10d6b000a4bf87b205196a1f12fd5b5c1dd
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2025-11-19 21:03:28 +0530

    fix:  templates/synapses_create_array.pyx

--- brian2/codegen/runtime/cython_rt/templates/synapses_create_array.pyx
+++ brian2/codegen/runtime/cython_rt/templates/synapses_create_array.pyx
@@ -1,16 +1,17 @@
 {# USES_VARIABLES { _synaptic_pre, _synaptic_post, sources, targets, N,
-                  N_incoming, N_outgoing,  N_pre, N_post, _source_offset, _target_offset }
+                    N_pre, N_post, _source_offset, _target_offset }
 #}
-{# WRITES_TO_READ_ONLY_VARIABLES { _synaptic_pre, _synaptic_post, N ,N_incoming, N_outgoing }
+{# WRITES_TO_READ_ONLY_VARIABLES { _synaptic_pre, _synaptic_post, N }
 #}
 {% extends 'common_group.pyx' %}
 
 {% block maincode %}
     cdef size_t _old_num_synapses = {{N}}
     cdef size_t _new_num_synapses = _old_num_synapses + _num{{sources}}
+
     # Resize the main synaptic connection arrays
-    {{_dynamic__synaptic_pre}}.resize(_new_num_synapses)
-    {{_dynamic__synaptic_post}}.resize(_new_num_synapses)
+    {{_dynamic__synaptic_pre_ptr}}.resize(_new_num_synapses)
+    {{_dynamic__synaptic_post_ptr}}.resize(_new_num_synapses)
     # Get the potentially newly created underlying data arrays
     cdef int32_t* _synaptic_pre_data = {{_dynamic__synaptic_pre_ptr}}.get_data_ptr()
     cdef int32_t* _synaptic_post_data = {{_dynamic__synaptic_post_ptr}}.get_data_ptr()
@@ -27,9 +28,8 @@
     # now we need to resize all registered variables and set the total number
     # of synapses without python indirection
     {% for varname in _registered_variables | variables_to_array_names(access_data=False) | sort %}
-    {{varname}}.resize(_new_num_synapses)
+    {{varname}}_ptr.resize(_new_num_synapses)
     {% endfor %}
-
     # Update the total number of synapses
     {{N}} = _new_num_synapses
 
